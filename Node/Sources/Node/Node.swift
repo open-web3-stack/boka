7fe3598839956e2006b945aba62b5c687849312d
Bryan Chen <xlchen1291@gmail.com>
2024-06-01 22:34:11 +1200

    setup

---
+++ Node/Sources/Node/Node.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public class Node {
+    public init() {
+    }
+    public func sayHello() {
+        print("Hello, World!")
+    }
+}
