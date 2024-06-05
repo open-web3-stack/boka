21b166a481b59256008b37a24bbd329e5787478f
Bryan Chen <xlchen1291@gmail.com>
2024-06-05 13:47:55 +1200

    header defined

--- Blockchain/Sources/Blockchain/Types/Header.swift
+++ Blockchain/Sources/Blockchain/Types/Header.swift
@@ -17,23 +17,35 @@ public struct Header {
     // the header’s epoch marker He is either empty or, if the block is the first in a new epoch,
     // then a tuple of the epoch randomness and a sequence of Bandersnatch keys
     // defining the Bandersnatch validator keys (kb) beginning in the next epoch
-    public private(set) var epoch: (randomness: H256, keys: [BandersnatchPublicKey])?
+    public private(set) var epoch: (
+        randomness: H256,
+        keys: SizeLimitedArray<
+            BandersnatchPublicKey,
+            Constants.TotalNumberOfValidators,
+            Constants.TotalNumberOfValidators
+        >
+    )?
 
     // Hw: winning-tickets
     // The winning-tickets marker Hw is either empty or,
     // if the block is the first after the end of the submission period
     // for tickets and if the ticket accumulator is saturated, then the final sequence of ticket identifiers
-    public private(set) var winningTickets: () // @TODO: figure out the type
+    public private(set) var winningTickets: SizeLimitedArray<
+        Ticket,
+        Constants.EpochLength,
+        Constants.EpochLength
+    >?
 
-    // Hj: judgements markers
-    public private(set) var judgementsMarkers: () // @TODO: figure out the type
+    // Hj: The judgement marker must contain exactly the sequence of report hashes judged not as
+    // confidently valid (i.e. either controversial or invalid).
+    public private(set) var judgementsMarkers: [H256]
 
-    // Hk: a Bandersnatch block author key Hk and two Bandersnatch signatures
-    public private(set) var authorKey: () // @TODO: figure out the type
+    // Hk: a Bandersnatch block author key Hk
+    public private(set) var authorKey: BandersnatchPublicKey
 
     // Hv: the entropy-yielding vrf signature
-    public private(set) var vrfSignature: () // @TODO: figure out the type
+    public private(set) var vrfSignature: BandersnatchSignature
 
     // Hs: block seal
-    public private(set) var seal: () // @TODO: figure out the type
+    public private(set) var seal: BandersnatchSignature
 }
---
+++ Utils/Sources/Utils/FixedSizeData.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+public struct FixedSizeData<T: ConstInt> {
+    // @TODO: completly hide data and only allow access via protocol methods
+    public private(set) var data: Data
+
+    public init?(_ value: Data) {
+        guard value.count == T.value else {
+            return nil
+        }
+        data = value
+    }
+
+    public init() {
+        data = Data(repeating: 0, count: T.value)
+    }
+}
+
+extension FixedSizeData: Equatable, Hashable {}
+
+extension FixedSizeData: CustomStringConvertible, CustomDebugStringConvertible {
+    public var description: String {
+        "0x\(data.map { String(format: "%02x", $0) }.joined())"
+    }
+
+    public var debugDescription: String {
+        description
+    }
+}
+
+public enum ConstInt32: ConstInt {
+    public static var value: Int {
+        32
+    }
+}
+
+public enum ConstInt64: ConstInt {
+    public static var value: Int {
+        64
+    }
+}
+
+public enum ConstUInt96: ConstInt {
+    public static var value: Int {
+        96
+    }
+}
+
+public typealias Data32 = FixedSizeData<ConstInt32>
+public typealias Data64 = FixedSizeData<ConstInt64>
+public typealias Data96 = FixedSizeData<ConstUInt96>
