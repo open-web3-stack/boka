7d77dd9678bee72119c9827d73f1f77426e38aa4
Qiwei Yang <yangqiwei97@gmail.com>
2024-06-25 15:08:01 +0800

    fix

--- Utils/Tests/UtilsTests/Blake2Tests.swift
+++ Utils/Tests/UtilsTests/Blake2Tests.swift
@@ -9,7 +9,7 @@ final class Blake2Tests: XCTestCase {
         let expected = hexStringToData("928b20366943e2afd11ebc0eae2e53a93bf177a4fcf35bcc64d503704e65e202")
         do {
             let actual = try blake2b256(testData)
-            XCTAssertEqual(expected, actual?.data)
+            XCTAssertEqual(expected, actual.data)
         } catch {
             XCTFail("Hashing failed")
         }
