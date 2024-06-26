a8ddae87c26d9185c10f04d50979a86483e720a5
Qiwei Yang <yangqiwei97@gmail.com>
2024-06-26 14:17:02 +0800

    fix

--- Utils/Sources/Utils/hashing.swift
+++ Utils/Sources/Utils/hashing.swift
@@ -1,10 +1,6 @@
 import Blake2
 import Foundation
 
-public enum HashConversionError: Error {
-    case invalidSize
-}
-
 /// Computes a Blake2b 256-bit hash of the input data and returns the result as a `Data32`.
 ///
 /// - Parameter: The input data to hash.
@@ -12,8 +8,6 @@ public enum HashConversionError: Error {
 /// - Returns: A `Data32` containing the hash result.
 public func blake2b256(_ data: Data) throws -> Data32 {
     let hash = try Blake2b.hash(size: 32, data: data)
-    guard let data32 = Data32(hash) else {
-        throw HashConversionError.invalidSize
-    }
+    let data32 = Data32(hash)!
     return data32
 }
--- Utils/Tests/UtilsTests/Blake2Tests.swift
+++ Utils/Tests/UtilsTests/Blake2Tests.swift
@@ -3,14 +3,10 @@ import XCTest
 @testable import Utils
 
 final class Blake2Tests: XCTestCase {
-    func testBlake2b256Test() {
+    func testBlake2b256Test() throws {
         let testData = Data("test".utf8)
         let expected = hexStringToData("928b20366943e2afd11ebc0eae2e53a93bf177a4fcf35bcc64d503704e65e202")
-        do {
-            let actual = try blake2b256(testData)
-            XCTAssertEqual(expected, actual.data)
-        } catch {
-            XCTFail("Hashing failed")
-        }
+        let actual = try blake2b256(testData)
+        XCTAssertEqual(expected, actual.data)
     }
 }
