70c52184211b6d4b60408934c7e8d6ba714ed55b
Qiwei Yang <yangqiwei97@gmail.com>
2024-06-26 14:22:32 +0800

    extension for data

--- Utils/Sources/Utils/Helpers.swift
+++ Utils/Sources/Utils/Helpers.swift
@@ -1,20 +1,25 @@
 import Foundation
 
-public func hexStringToData(_ hexString: String) -> Data? {
-    var data = Data()
-    var index = hexString.startIndex
-
-    while index < hexString.endIndex {
-        // Find the next 2 characters (1 byte)
-        guard let nextIndex = hexString.index(index, offsetBy: 2, limitedBy: hexString.endIndex),
-              let byte = UInt8(hexString[index ..< nextIndex], radix: 16)
-        else {
+extension Data {
+    init?(fromHexString hexString: String) {
+        guard !hexString.isEmpty else {
             return nil
         }
 
-        data.append(byte)
-        index = nextIndex
-    }
+        var data = Data()
+        var index = hexString.startIndex
 
-    return data
+        while index < hexString.endIndex {
+            guard let nextIndex = hexString.index(index, offsetBy: 2, limitedBy: hexString.endIndex),
+                  let byte = UInt8(hexString[index ..< nextIndex], radix: 16)
+            else {
+                return nil
+            }
+
+            data.append(byte)
+            index = nextIndex
+        }
+
+        self.init(data)
+    }
 }
--- Utils/Tests/UtilsTests/Blake2Tests.swift
+++ Utils/Tests/UtilsTests/Blake2Tests.swift
@@ -5,7 +5,7 @@ import XCTest
 final class Blake2Tests: XCTestCase {
     func testBlake2b256Test() throws {
         let testData = Data("test".utf8)
-        let expected = hexStringToData("928b20366943e2afd11ebc0eae2e53a93bf177a4fcf35bcc64d503704e65e202")
+        let expected = Data(fromHexString: "928b20366943e2afd11ebc0eae2e53a93bf177a4fcf35bcc64d503704e65e202")
         let actual = try blake2b256(testData)
         XCTAssertEqual(expected, actual.data)
     }
