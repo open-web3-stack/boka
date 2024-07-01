4429943b93f8cf828ffaef6ce2622848578971bc
Bryan Chen <xlchen1291@gmail.com>
2024-07-02 10:28:23 +1200

    rename

--- Blockchain/Sources/Blockchain/Types/ValidatorKey.swift
+++ Blockchain/Sources/Blockchain/Types/ValidatorKey.swift
@@ -2,20 +2,20 @@ import ScaleCodec
 import Utils
 
 public struct ValidatorKey: Sendable {
-    public var bandersnatchKey: BandersnatchPublicKey
-    public var ed25519Key: Ed25519PublicKey
-    public var blsKey: BLSKey
+    public var bandersnatch: BandersnatchPublicKey
+    public var ed25519: Ed25519PublicKey
+    public var bls: BLSKey
     public var metadata: Data128
 
     public init(
-        bandersnatchKey: BandersnatchPublicKey,
-        ed25519Key: Ed25519PublicKey,
-        blsKey: BLSKey,
+        bandersnatch: BandersnatchPublicKey,
+        ed25519: Ed25519PublicKey,
+        bls: BLSKey,
         metadata: Data128
     ) {
-        self.bandersnatchKey = bandersnatchKey
-        self.ed25519Key = ed25519Key
-        self.blsKey = blsKey
+        self.bandersnatch = bandersnatch
+        self.ed25519 = ed25519
+        self.bls = bls
         self.metadata = metadata
     }
 }
@@ -24,9 +24,9 @@ extension ValidatorKey: Dummy {
     public typealias Config = ProtocolConfigRef
     public static func dummy(withConfig _: Config) -> ValidatorKey {
         ValidatorKey(
-            bandersnatchKey: BandersnatchPublicKey(),
-            ed25519Key: Ed25519PublicKey(),
-            blsKey: BLSKey(),
+            bandersnatch: BandersnatchPublicKey(),
+            ed25519: Ed25519PublicKey(),
+            bls: BLSKey(),
             metadata: Data128()
         )
     }
@@ -35,17 +35,17 @@ extension ValidatorKey: Dummy {
 extension ValidatorKey: ScaleCodec.Codable {
     public init(from decoder: inout some ScaleCodec.Decoder) throws {
         try self.init(
-            bandersnatchKey: decoder.decode(),
-            ed25519Key: decoder.decode(),
-            blsKey: decoder.decode(),
+            bandersnatch: decoder.decode(),
+            ed25519: decoder.decode(),
+            bls: decoder.decode(),
             metadata: decoder.decode()
         )
     }
 
     public func encode(in encoder: inout some ScaleCodec.Encoder) throws {
-        try encoder.encode(bandersnatchKey)
-        try encoder.encode(ed25519Key)
-        try encoder.encode(blsKey)
+        try encoder.encode(bandersnatch)
+        try encoder.encode(ed25519)
+        try encoder.encode(bls)
         try encoder.encode(metadata)
     }
 }
