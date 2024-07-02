ea50f04e3c9322dad450014451bdeddf610e2bed
Qiwei Yang <yangqiwei97@gmail.com>
2024-07-02 20:51:45 +0800

    try 0.10.0

--- Utils/Package.swift
+++ Utils/Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .package(url: "https://github.com/tesseract-one/ScaleCodec.swift.git", from: "0.3.0"),
         .package(url: "https://github.com/tesseract-one/Blake2.swift.git", from: "0.2.0"),
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "4.0.0"),
-        .package(url: "https://github.com/apple/swift-testing.git", branch: "main"),
+        .package(url: "https://github.com/apple/swift-testing.git", branch: "0.10.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
