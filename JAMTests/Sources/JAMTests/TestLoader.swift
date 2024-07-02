b3a0195f212c07a73a1eddb27f84be6b26dc73b5
Bryan Chen <xlchen1291@gmail.com>
2024-07-02 21:40:42 +1200

    update

--- JAMTests/Sources/JAMTests/TestLoader.swift
+++ JAMTests/Sources/JAMTests/TestLoader.swift
@@ -1,26 +1,13 @@
 import Foundation
 
-#if compiler(>=6.0)
-    extension TestLoader {
-        func test() {
-            #error(">= 6.0")
-        }
-    }
-#else
-    extension TestLoader {
-        func test() {
-            #error("< 6.0")
-        }
-    }
-#endif
+extension Foundation.Bundle: @unchecked @retroactive Sendable {}
 
 enum TestLoader {
-    static func getTestFiles(forPath path: String) throws -> [String] {
+    static func getTestFiles(path: String, extension ext: String) throws -> [String] {
         let prefix = Bundle.module.bundlePath + "/jamtestvectors/\(path)"
-        print("Discovering tests in \(prefix)")
         let files = try FileManager.default.contentsOfDirectory(atPath: prefix)
-        var scaleFiles = files.filter { $0.hasSuffix(".scale") }
-        scaleFiles.sort()
-        return scaleFiles.map { prefix + "/" + $0 }
+        var filtered = files.filter { $0.hasSuffix(".\(ext)") }
+        filtered.sort()
+        return filtered.map { prefix + "/" + $0 }
     }
 }
