a9d98950dfb374647534b36671756cb84b81e3bd
Bryan Chen <xlchen1291@gmail.com>
2024-07-02 17:27:52 +1200

    update

--- JAMTests/Sources/JAMTests/TestLoader.swift
+++ JAMTests/Sources/JAMTests/TestLoader.swift
@@ -1,5 +1,19 @@
 import Foundation
 
+#if compiler(>=6.0)
+    extension TestLoader {
+        func test() {
+            #error(">= 6.0")
+        }
+    }
+#else
+    extension TestLoader {
+        func test() {
+            #error("< 6.0")
+        }
+    }
+#endif
+
 enum TestLoader {
     static func discover(forPath path: String) throws {
         let prefix = Bundle.module.bundlePath + "/jamtestvectors/\(path)"
