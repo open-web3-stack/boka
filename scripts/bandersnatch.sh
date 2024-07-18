873cbbe1729846d8b1f9456eb3f7f9b48f3d6ab1
Qiwei Yang <yangqiwei97@gmail.com>
2024-07-18 10:41:26 +0800

    fix setup

--- scripts/bandersnatch.sh
+++ scripts/bandersnatch.sh
@@ -4,21 +4,14 @@ set -e
 
 
 # Setup bandersnatch vrf c binding
-cd "$(dirname "$0")/../Utils/Sources/bandersnatch" || { echo "directory not found"; exit 1; }
+CWD=$(pwd)
 
-mkdir -p include
-mkdir -p lib
+mkdir -p .lib
+
+cd Utils/Sources/bandersnatch || { echo "directory not found"; exit 1; }
 
 cargo build --lib
 
-cp target/debug/libbandersnatch_vrfs.a lib
-
-cat <<EOL > include/module.modulemap
-module bandersnatch_vrfs {
-  header "./bindings.h"
-  link "bandersnatch_vrfs"
-  export *
-}
-EOL
+cp target/debug/libbandersnatch_vrfs.a ${CWD}/.lib
 
 echo "Setup bandersnatch_vrfs successfully."
