217c6245587cc199a191dc3da8c024f980a284fe
Bryan Chen <xlchen1291@gmail.com>
2024-07-22 14:01:48 +1200

    more tests

--- PolkaVM/Tests/PolkaVMTests/ProgramTests.swift
+++ PolkaVM/Tests/PolkaVMTests/ProgramTests.swift
@@ -21,8 +21,31 @@ struct ProgramTests {
         #expect(throws: Program.Error.invalidJumpTableEncodeSize) { try Program(data) }
     }
 
+    @Test func invalidCodeLength() {
+        let highValue = Data(UInt64(0x1000000).encode(method: .variableWidth))
+        let data = Data([0, 0]) + highValue
+        #expect(throws: Program.Error.invalidCodeLength) { try Program(data) }
+    }
+
+    @Test func tooMuchData() throws {
+        let data = Data([0, 0, 2, 1, 2, 0, 0])
+        #expect(throws: Program.Error.invalidDataLength) { try Program(data) }
+    }
+
+    @Test func tooLittleData() throws {
+        let data = Data([0, 0, 2, 1, 2])
+        #expect(throws: Program.Error.invalidDataLength) { try Program(data) }
+    }
+
     @Test func minimal() throws {
         let data = Data([0, 0, 0])
         _ = try Program(data)
     }
+
+    @Test func simple() throws {
+        let data = Data([0, 0, 2, 1, 2, 0])
+        _ = try Program(data)
+    }
+
+    // TODO: add more tests
 }
