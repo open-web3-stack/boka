33d9721fc152952ca08e3e430f8ad01ffd587d66
Qiwei Yang <yangqiwei97@gmail.com>
2024-08-02 11:10:33 +0800

    remove format

--- Database/Tests/DatabaseTests/RocksDBTests.swift
+++ Database/Tests/DatabaseTests/RocksDBTests.swift
@@ -1,4 +1,5 @@
 // swiftlint:disable force_try
+// swiftformat:disable hoistTry
 import Foundation
 import Testing
 
@@ -29,21 +30,21 @@ final class RocksDBTests {
     }
 
     @Test func basicOperations() throws {
-        #expecttry (rocksDB.get(key: "123".data) == nil)
+        #expect(try rocksDB.get(key: "123".data) == nil)
 
         try rocksDB.put(key: "123".data, value: "qwe".data)
         try rocksDB.put(key: "234".data, value: "asd".data)
 
-        #expecttry (rocksDB.get(key: "123".data) == "qwe".data)
-        #expecttry (rocksDB.get(key: "234".data) == "asd".data)
+        #expect(try rocksDB.get(key: "123".data) == "qwe".data)
+        #expect(try rocksDB.get(key: "234".data) == "asd".data)
 
         try rocksDB.delete(key: "123".data)
 
-        #expecttry (rocksDB.get(key: "123".data) == nil)
+        #expect(try rocksDB.get(key: "123".data) == nil)
 
         try rocksDB.put(key: "234".data, value: "asdfg".data)
 
-        #expecttry (rocksDB.get(key: "234".data) == "asdfg".data)
+        #expect(try rocksDB.get(key: "234".data) == "asdfg".data)
     }
 
     @Test func testBatchOperations() throws {
@@ -57,8 +58,8 @@ final class RocksDBTests {
             .put(key: "345".data, value: "ertert".data),
         ])
 
-        #expecttry (rocksDB.get(key: "123".data) == nil)
-        #expecttry (rocksDB.get(key: "234".data) == nil)
-        #expecttry (rocksDB.get(key: "345".data) == "ertert".data)
+        #expect(try rocksDB.get(key: "123".data) == nil)
+        #expect(try rocksDB.get(key: "234".data) == nil)
+        #expect(try rocksDB.get(key: "345".data) == "ertert".data)
     }
 }
