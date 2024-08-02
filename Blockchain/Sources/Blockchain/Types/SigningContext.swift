195e340e90d13f4f2f0a6a61de1933aae60b6445
Qiwei Yang <yangqiwei97@gmail.com>
2024-08-02 12:57:48 +0800

    use data

--- Blockchain/Sources/Blockchain/Types/SigningContext.swift
+++ Blockchain/Sources/Blockchain/Types/SigningContext.swift
@@ -1,51 +1,53 @@
+import Foundation
+
 public enum SigningContext {
     /// XA = $jam_available: Ed25519 Availability assurances.
-    static var available: String {
-        "jam_available"
+    static var available: Data {
+        Data("jam_available".utf8)
     }
 
     /// XB = $jam_beefy: bls Accumulate-result-root-mmr commitment.
-    static var beefy: String {
-        "jam_beefy"
+    static var beefy: Data {
+        Data("jam_beefy".utf8)
     }
 
     /// XE = $jam_entropy: On-chain entropy generation.
-    static var entropy: String {
-        "jam_entropy"
+    static var entropy: Data {
+        Data("jam_entropy".utf8)
     }
 
     /// XF = $jam_fallback_seal: Bandersnatch Fallback block seal.
-    static var fallbackSeal: String {
-        "jam_fallback_seal"
+    static var fallbackSeal: Data {
+        Data("jam_fallback_seal".utf8)
     }
 
     /// XG = $jam_guarantee: Ed25519 Guarantee statements.
-    static var guarantee: String {
-        "jam_guarantee"
+    static var guarantee: Data {
+        Data("jam_guarantee".utf8)
     }
 
     /// XI = $jam_announce: Ed25519 Audit announcement statements.
-    static var announce: String {
-        "jam_announce"
+    static var announce: Data {
+        Data("jam_announce".utf8)
     }
 
     /// XT = $jam_ticket_seal: Bandersnatch Ringvrf Ticket generation and regular block seal.
-    static var ticketSeal: String {
-        "jam_ticket_seal"
+    static var ticketSeal: Data {
+        Data("jam_ticket_seal".utf8)
     }
 
     /// XU = $jam_audit: Bandersnatch Audit selection entropy.
-    static var audit: String {
-        "jam_audit"
+    static var audit: Data {
+        Data("jam_audit".utf8)
     }
 
     /// X_top = $jam_valid: Ed25519 Judgements for valid work-reports.
-    static var valid: String {
-        "jam_valid"
+    static var valid: Data {
+        Data("jam_valid".utf8)
     }
 
     /// X_bot = $jam_invalid: Ed25519 Judgements for invalid work-reports.
-    static var invalid: String {
-        "jam_invalid"
+    static var invalid: Data {
+        Data("jam_invalid".utf8)
     }
 }
