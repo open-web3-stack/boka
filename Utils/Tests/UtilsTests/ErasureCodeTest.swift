7ab72b21e56a6d8234120ae781776e383009518f
Qiwei Yang <yangqiwei97@gmail.com>
2024-08-09 14:30:55 +0800

    fix

--- Utils/Tests/UtilsTests/ErasureCodeTest.swift
+++ Utils/Tests/UtilsTests/ErasureCodeTest.swift
@@ -1,9 +1,10 @@
 import Foundation
+import Testing
+
+@testable import Utils
 
 // somehow without this the GH Actions CI fails
 extension Foundation.Bundle: @unchecked @retroactive Sendable {}
-
-@testable import Utils
 
 @Suite struct ErasureCodeTests {
     @Test func testReconstruct() throws {}
