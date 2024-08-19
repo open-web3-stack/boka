a99a6ae2333637b5f3d224b17abda6a0f8003d3a
MacOMNI <414294494@qq.com>
2024-08-19 17:43:07 +0800

    Dev mackun (#64)
    * #update  ignore
    
    * try to fix ExtrinsicPreimages issue
    
    * fix fixedSizeData issue
    
    * try to fix ExtrinsicPreimages issue
    
    * #update Preimages serviceIndices

--- Blockchain/Sources/Blockchain/Types/primitives.swift
+++ Blockchain/Sources/Blockchain/Types/primitives.swift
@@ -4,7 +4,9 @@ public typealias Balances = UInt64
 public typealias ServiceIdentifier = Data32
 public typealias TimeslotIndex = UInt32
 public typealias Gas = UInt64
+public typealias ServiceIndices = UInt32
 public typealias DataLength = UInt32
+
 public typealias ValidatorIndex = UInt32 // TODO: confirm this
 public typealias CoreIndex = UInt32 // TODO: confirm this
 public typealias TicketIndex = UInt8
