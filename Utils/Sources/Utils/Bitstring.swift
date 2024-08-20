7abcf38f3da110918d31a5a805004edfdac1ee9d
MacOMNI <414294494@qq.com>
2024-08-20 16:31:33 +0800

    update Bitstring & test

--- Utils/Sources/Utils/Bitstring.swift
+++ Utils/Sources/Utils/Bitstring.swift
@@ -1,11 +1,15 @@
 import Foundation
+import ScaleCodec
+public enum BitError: Error, Equatable {
+    case custom(String)
+}
 
 public struct Bitstring: Hashable, Sendable {
 
   /// Total number of bits.
-  public let length: Int
+  public private(set) var length: Int
   /// Byte storage for bits.
-  public let bytes: Data
+  public private(set) var bytes: Data
 
   /// Initialize with total bit length and byte storage.
   ///
@@ -31,21 +35,27 @@ public struct Bitstring: Hashable, Sendable {
     length = bytes.count * 8
   }
 
+  public init(length: Int) {
+    self.length = length
+    let byteCount = (length + 7) / 8
+    self.bytes = Data(repeating: 0, count: byteCount)
+  }
+
   /// Initialize with binaryString.
   ///
   /// - Parameter bitString: String of bit array. Each character of the string must be a `1` or `0`.
   /// - Returns: Initialized ``BitString`` or `nil` if any character of the string is not a `1` or `0`.
   ///
-  public init?(_ binaryString: String) {
+  public init(_ binaryString: String) throws {
     // Ensure the string only contains '0' and '1'
     guard binaryString.allSatisfy({ $0 == "0" || $0 == "1" }) else {
-      return nil
+       throw BitError.custom("Bitstring must contain only 0s and 1s.")
     }
     let length = binaryString.count
-    var bytes = Data(repeating: 0, count: (length + 7) / 8)  // +7 to round up to the nearest byte
+    var bytes: Data = Data(repeating: 0, count: (length + 7) / 8)  // +7 to round up to the nearest byte
     for (index, char) in binaryString.enumerated() {
       if char == "1" {
-        let byteIndex = index / 8
+        let byteIndex = index >> 3
         let bitIndex = 7 - (index % 8)
         bytes[byteIndex] |= (1 << bitIndex)
       }
@@ -53,15 +63,71 @@ public struct Bitstring: Hashable, Sendable {
     self.init(length: length, bytes: bytes)
   }
 
+  func at(unchecked index: Int) -> Int {
+    let byteIndex = index >> 3
+    let bitIndex = 7 - (index) % 8
+    return (bytes[byteIndex] & (1 << bitIndex)) != 0 ? 1 : 0
+  }
+
   /// String of bit array as text representation with each character a `0` or `1`.
   public var bitString: String {
     return bytes.map { ($0 != 0) ? "1" : "0" }.joined(separator: "")
+  }
+
+  /// Formats the bitstring in binary digits.
+  public var binaryString: String {
+    var s = ""
+    for i in 0..<length {
+      s.append(at(unchecked: i) == 1 ? "1" : "0")
+    }
+    return s
   }
 
   public var description: String { bitString }
 
 }
 
+extension Bitstring: Comparable {
+  public static func < (lhs: Self, rhs: Self) -> Bool {
+    for i in 0..<min(lhs.length, rhs.length) {
+      let l = lhs.at(unchecked: i)
+      let r = rhs.at(unchecked: i)
+      if l == 0 && r == 1 { return true }
+      if l == 1 && r == 0 { return false }
+    }
+    return lhs.length <= rhs.length  // shorter string comes first, tie is in favor of the LHS
+  }
+}
+
 extension Bitstring: Equatable {
 
+  /**
+     Checks for equality
+    - parameter lhs: bitstring
+    - parameter rhs: bitstring
+    - returns true if the bitstrings are equal, false otherwise
+     */
+  public static func == (lhs: Bitstring, rhs: Bitstring) -> Bool {
+    if lhs.length != rhs.length {
+      return false
+    }
+    for i in 0..<lhs.length {
+      let lhsI = lhs.at(unchecked: i)
+      let rhsI = rhs.at(unchecked: i)
+      if lhsI != rhsI {
+        return false
+      }
+    }
+    return true
+  }
+}
+
+extension Bitstring: ScaleCodec.Codable {
+    public init(from decoder: inout some ScaleCodec.Decoder) throws {
+        try self.init(decoder.decode(String.self))
+    }
+
+    public func encode(in encoder: inout some ScaleCodec.Encoder) throws {
+        try encoder.encode(binaryString)
+    }
 }
--- Utils/Tests/UtilsTests/BitStringTests.swift
+++ Utils/Tests/UtilsTests/BitStringTests.swift
@@ -8,18 +8,32 @@ import Testing
         let binaryString = "01"
         let expectedLength = 2
         let expectedBytes = Data([0b01000000]) // "01" -> 0b01000000 in a single byte
-        let bitString = Bitstring(binaryString)
-        #expect(expectedBytes == bitString!.bytes)
-        #expect(expectedLength == bitString!.length)
-
+        let bitString = try Bitstring(binaryString)
+        #expect(expectedBytes == bitString.bytes)
+        #expect(expectedLength == bitString.length)
     }
     @Test func testBitString_100000001() throws {
         let binaryString: String = "100000001"
         let expectedLength = 9
         let expectedBytes = Data([0b10000000, 0b10000000]) // "100000001" -> 0b10000000 0b10000000 in two bytes
-        if let bitstring = Bitstring(binaryString) {
-            #expect(bitstring.length == expectedLength)
-            #expect(bitstring.bytes == expectedBytes)
+        let bitstring = try Bitstring(binaryString) 
+        #expect(bitstring.length == expectedLength)
+        #expect(bitstring.bytes == expectedBytes)
+        #expect(bitstring.binaryString == binaryString)
+}
+    @Test func testInvalidBinaryString() throws {
+        let invalidBinaryString = "02"
+        do {
+            _ = try Bitstring(invalidBinaryString)
+        } catch {
+            print(error)
+            #expect(true)
         }
     }
+    @Test func testinitBinaryStringWithLength() throws {
+        let bitstring = Bitstring(length: 341)
+        print(bitstring.bitString)
+        print(bitstring.binaryString)
+    }
+    
 }
