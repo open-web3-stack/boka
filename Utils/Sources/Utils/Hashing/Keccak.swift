7400a4ae053c7e19e6c0c3eade4fec6faa54f9b4
Xiliang Chen <xlchen1291@gmail.com>
2024-08-26 17:42:34 +1200

    add keccak and move some files around (#79)
    * add keccak and move some files around
    
    * fix

---
+++ Utils/Sources/Utils/Hashing/Keccak.swift
@@ -0,0 +1,24 @@
+import Blake2
+import Foundation
+import sha3_iuf
+
+public struct Keccak: ~Copyable, Hashing {
+    private var ctx: sha3_context = .init()
+
+    public init() {
+        sha3_Init256(&ctx)
+        sha3_SetFlags(&ctx, SHA3_FLAGS_KECCAK)
+    }
+
+    public mutating func update(_ data: some DataPtrRepresentable) {
+        data.withPtr { ptr in
+            sha3_Update(&ctx, ptr.baseAddress, ptr.count)
+        }
+    }
+
+    public consuming func finalize() -> Data32 {
+        let ptr = sha3_Finalize(&ctx)!
+        let data = Data(bytes: ptr, count: 32)
+        return Data32(data)!
+    }
+}
