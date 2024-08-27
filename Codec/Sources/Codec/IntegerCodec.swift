1ab56a2f6e132b409e5dcc96fb4b91102bd3370f
Bryan Chen <xlchen1291@gmail.com>
2024-08-27 17:59:41 +1200

    only use scale for decoder

--- Codec/Sources/Codec/IntegerCodec.swift
+++ Codec/Sources/Codec/IntegerCodec.swift
@@ -20,14 +20,6 @@ extension Collection<UInt8> where SubSequence == Self {
 }
 
 public enum IntegerCodec {
-    public enum DecodeMode {
-        case jam
-        case scale
-    }
-
-    // TODO: remove scale codec after JAM test vectors are updated
-    public nonisolated(unsafe) static var decodeMode: DecodeMode = .jam
-
     public static func decode<T: UnsignedInteger>(length: Int, next: () throws -> UInt8?) rethrows -> T? {
         guard length > 0 else {
             return nil
@@ -48,34 +40,6 @@ public enum IntegerCodec {
         }
         if firstByte == 0 {
             return 0
-        }
-
-        if decodeMode == .scale {
-            switch firstByte & 0b11 {
-            case 0b00: // 1 byte
-                return UInt64(firstByte >> 2)
-            case 0b01: // 2 bytes
-                guard let secondByte = try next() else {
-                    return nil
-                }
-                return UInt64(firstByte >> 2) | (UInt64(secondByte) << 6)
-            case 0b10: // 4 bytes
-                guard let secondByte = try next() else {
-                    return nil
-                }
-                guard let thirdByte = try next() else {
-                    return nil
-                }
-                guard let fourthByte = try next() else {
-                    return nil
-                }
-                let value = UInt64(firstByte >> 2) | (UInt64(secondByte) << 6) | (UInt64(thirdByte) << 14) | (UInt64(fourthByte) << 22)
-                return value
-            case 0b11: // variable bytes
-                fatalError("variable bytes compact codec not implemented")
-            default:
-                fatalError("unreachable")
-            }
         }
 
         let byteLengh = (~firstByte).leadingZeroBitCount
