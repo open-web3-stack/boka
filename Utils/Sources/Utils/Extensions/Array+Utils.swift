fda8c2024e5de6e493fbe3fed3bf4a996d8f24a7
Bryan Chen <xlchen1291@gmail.com>
2024-08-30 11:42:35 +1200

    implements disputes protocol

--- Utils/Sources/Utils/Extensions/Array+Utils.swift
+++ Utils/Sources/Utils/Extensions/Array+Utils.swift
@@ -1,17 +1,4 @@
-extension Array where Element: Comparable {
-    public func isSorted(by comparer: (Element, Element) -> Bool = { $0 < $1 }) -> Bool {
-        var previous: Element?
-        for element in self {
-            if let previous {
-                if !comparer(previous, element), previous != element {
-                    return false
-                }
-            }
-            previous = element
-        }
-        return true
-    }
-
+extension Array {
     /// Insert the elements of the given sequence to the array, in sorted order.
     ///
     /// - Parameter elements: The elements to insert.
@@ -20,9 +7,8 @@ extension Array where Element: Comparable {
     ///
     /// - Note: The elements of the sequence must be comparable.
     /// - Invariant: The array and elements must be sorted according to the given comparison function.
-    public mutating func insertSorted(_ elements: any Sequence<Element>, by comparer: ((Element, Element) throws -> Bool)? = nil) rethrows {
+    public mutating func insertSorted(_ elements: any Sequence<Element>, by comparer: (Element, Element) throws -> Bool) rethrows {
         reserveCapacity(count + elements.underestimatedCount)
-        let comparer = comparer ?? { $0 < $1 }
         var startIdx = 0
         for element in elements {
             if let idx = try self[startIdx...].firstIndex(where: { try !comparer($0, element) }) {
@@ -33,5 +19,19 @@ extension Array where Element: Comparable {
                 startIdx = endIndex
             }
         }
+    }
+}
+
+extension Array where Element: Comparable {
+    /// Insert the elements of the given sequence to the array, in sorted order.
+    ///
+    /// - Parameter elements: The elements to insert.
+    /// - Parameter comparer: The comparison function to use to determine the order of the elements.
+    /// - Complexity: O(*n*), where *n* is the number of elements in the sequence.
+    ///
+    /// - Note: The elements of the sequence must be comparable.
+    /// - Invariant: The array and elements must be sorted according to the given comparison function.
+    public mutating func insertSorted(_ elements: any Sequence<Element>) {
+        insertSorted(elements) { $0 < $1 }
     }
 }
--- Utils/Tests/UtilsTests/ArrayTests.swift
+++ Utils/Tests/UtilsTests/ArrayTests.swift
@@ -5,27 +5,6 @@ import Testing
 
 struct ArrayTests {
     @Test(arguments: [
-        [], [1], [2, 4], [5, 19, 34, 34, 56, 56],
-    ])
-    func isSorted(testCase: [Int]) {
-        #expect(testCase.isSorted())
-    }
-
-    @Test(arguments: [
-        [], [1], [4, 2], [56, 56, 34, 34, 19, 5]
-    ])
-    func isSortedBy(testCase: [Int]) {
-        #expect(testCase.isSorted(by: >))
-    }
-
-    @Test(arguments: [
-        [4, 2], [56, 56, 34, 35, 19, 5], [1, 3, 2]
-    ])
-    func notSorted(testCase: [Int]) {
-        #expect(!testCase.isSorted())
-    }
-
-    @Test(arguments: [
         ([], []),
         ([], [1]),
         ([1], []),
