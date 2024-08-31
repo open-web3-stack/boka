c75082254b48fc8a19d9fe0858f38d2b3ef6dd0d
Qiwei Yang <yangqiwei97@gmail.com>
2024-08-31 09:03:04 +0800

    fix test logger

--- TracingUtils/Sources/TracingUtils/logger.swift
+++ TracingUtils/Sources/TracingUtils/logger.swift
@@ -1,8 +1,23 @@
+import Foundation
+
+// TODO: Idea from https://github.com/apple/swift-log/pull/316, we may use that when it's released
+
+// `nonisolated(unsafe)` added using compiler suggestion:
+//     Disable concurrency-safety checks if accesses are protected by an external synchronization mechanism
+// it's handled by `DispatchQueue` in this case
+private nonisolated(unsafe) var isInitialized: Bool = false
+private let loggerSetupQueue = DispatchQueue(label: "loggerSetupQueue")
+
 /// Setup a logger for testing purposes
 public func setupTestLogger(level: Logger.Level = .debug) {
-    LoggingSystem.bootstrap { label in
-        var handler = StreamLogHandler.standardError(label: label)
-        handler.logLevel = level
-        return handler
+    loggerSetupQueue.sync {
+        if !isInitialized {
+            LoggingSystem.bootstrap { label in
+                var handler = StreamLogHandler.standardError(label: label)
+                handler.logLevel = level
+                return handler
+            }
+            isInitialized = true
+        }
     }
 }
