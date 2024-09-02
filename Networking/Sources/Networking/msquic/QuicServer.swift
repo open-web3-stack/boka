0fd5bb7f8f0d1ea1236870a5e09bce0ccb43019e
MacOMNI <414294494@qq.com>
2024-09-02 19:22:28 +0800

    QuicServer update

--- Networking/Sources/Networking/msquic/QuicServer.swift
+++ Networking/Sources/Networking/msquic/QuicServer.swift
@@ -5,6 +5,10 @@ typealias ConnectionCallback = @convention(c) (
     OpaquePointer?, UnsafeMutableRawPointer?, UnsafePointer<QuicConnectionEvent>?
 ) -> QuicStatus
 
+typealias StreamCallback = @convention(c) (
+    OpaquePointer?, UnsafeMutableRawPointer?, UnsafeMutablePointer<QuicStreamEvent>?
+) -> QuicStatus
+
 typealias ServerListenerCallback = @convention(c) (
     OpaquePointer?, UnsafeMutableRawPointer?, UnsafeMutablePointer<QuicListenerEvent>?
 ) -> QuicStatus
@@ -63,12 +67,44 @@ public final class QuicServer {
         print("QuicServer Deinit")
     }
 
-    private static let serverListenerCallback: ServerListenerCallback = { listener, context, event in
+    private static let serverListenerCallback: ServerListenerCallback = {
+        listener, context, event in
         guard let context, let event else {
             return QuicStatusCode.notSupported.rawValue
         }
         let server = Unmanaged<QuicServer>.fromOpaque(context).takeUnretainedValue()
         return server.handleListenerEvent(listener, event)
+    }
+
+    private static let streamCallback: StreamCallback = { stream, context, event in
+        guard let context, let event else {
+            return QuicStatusCode.notSupported.rawValue
+        }
+        let server = Unmanaged<QuicServer>.fromOpaque(context).takeUnretainedValue()
+        var status: QuicStatus = QuicStatusCode.success.rawValue
+        switch event.pointee.Type {
+        case QUIC_STREAM_EVENT_SEND_COMPLETE:
+            if let clientContext = event.pointee.SEND_COMPLETE.ClientContext {
+                free(clientContext)
+            }
+            print("[strm][\(String(describing: stream))] Data sent")
+        case QUIC_STREAM_EVENT_RECEIVE:
+            print("[strm][\(String(describing: stream))] Data received")
+        case QUIC_STREAM_EVENT_PEER_SEND_SHUTDOWN:
+            print("[strm][\(String(describing: stream))] Peer shut down")
+        // serverSend(stream)
+        case QUIC_STREAM_EVENT_PEER_SEND_ABORTED:
+            print("[strm][\(String(describing: stream))] Peer aborted")
+            status =
+                (server.api?.pointee.StreamShutdown(stream, QUIC_STREAM_SHUTDOWN_FLAG_ABORT, 0))
+                    .status
+        case QUIC_STREAM_EVENT_SHUTDOWN_COMPLETE:
+            print("[strm][\(String(describing: stream))] All done")
+            server.api?.pointee.StreamClose(stream)
+        default:
+            break
+        }
+        return status
     }
 
     private static let connectionCallback: ConnectionCallback = { _, context, event in
@@ -87,21 +123,28 @@ public final class QuicServer {
             print("Connection shutdown complete.")
         case QUIC_CONNECTION_EVENT_PEER_STREAM_STARTED:
             let stream = event.pointee.PEER_STREAM_STARTED.Stream
-        // server.api?.pointee.SetCallbackHandler(
-        //     stream, streamCallback,
-        //     UnsafeMutableRawPointer(Unmanaged.passUnretained(server).toOpaque()))
+            let streamCallbackPointer = UnsafeMutablePointer<StreamCallback>.allocate(capacity: 1)
+            streamCallbackPointer.initialize(to: QuicServer.streamCallback)
+            server.api?.pointee.SetCallbackHandler(
+                stream, streamCallbackPointer,
+                UnsafeMutableRawPointer(Unmanaged.passUnretained(server).toOpaque())
+            )
         default:
             break
         }
         return QuicStatusCode.success.rawValue
     }
 
-    private func handleListenerEvent(_: OpaquePointer?, _ event: UnsafePointer<QuicListenerEvent>) -> QuicStatus {
+    private func handleListenerEvent(_: OpaquePointer?, _ event: UnsafePointer<QuicListenerEvent>)
+        -> QuicStatus
+    {
         var status: QuicStatus = QuicStatusCode.success.rawValue
         switch event.pointee.Type {
         case QUIC_LISTENER_EVENT_NEW_CONNECTION:
             let connection = event.pointee.NEW_CONNECTION.Connection
-            let connectionCallbackPointer = UnsafeMutablePointer<ConnectionCallback>.allocate(capacity: 1)
+            let connectionCallbackPointer = UnsafeMutablePointer<ConnectionCallback>.allocate(
+                capacity: 1
+            )
             connectionCallbackPointer.initialize(to: QuicServer.connectionCallback)
             api?.pointee.SetCallbackHandler(
                 connection,
@@ -116,32 +159,6 @@ public final class QuicServer {
     }
 
     private func handleConnectionEvent(_: OpaquePointer?, _: UnsafePointer<QuicConnectionEvent>) {}
-
-    static let streamCallback:
-        @convention(c) (OpaquePointer?, UnsafeMutableRawPointer?, UnsafePointer<QuicStreamEvent>?)
-        -> UInt32 = { _, _, event in
-            guard let event else { return QuicStatusCode.invalidParameter.rawValue }
-
-            switch event.pointee.Type {
-            case QUIC_STREAM_EVENT_SEND_COMPLETE:
-                print("Stream send complete.")
-                return QuicStatusCode.success.rawValue
-            case QUIC_STREAM_EVENT_RECEIVE:
-                print("Stream data received.")
-                return QuicStatusCode.success.rawValue
-            case QUIC_STREAM_EVENT_PEER_SEND_SHUTDOWN:
-                print("Stream peer send shutdown.")
-                return QuicStatusCode.success.rawValue
-            case QUIC_STREAM_EVENT_PEER_SEND_ABORTED:
-                print("Stream peer send aborted.")
-                return QuicStatusCode.success.rawValue
-            case QUIC_STREAM_EVENT_SHUTDOWN_COMPLETE:
-                print("Stream shutdown complete.")
-                return QuicStatusCode.success.rawValue
-            default:
-                return QuicStatusCode.invalidParameter.rawValue
-            }
-        }
 }
 
 extension QuicServer {
@@ -156,8 +173,7 @@ extension QuicServer {
         var credConfig = QUIC_CREDENTIAL_CONFIG()
         memset(&credConfig, 0, MemoryLayout.size(ofValue: credConfig))
         credConfig.Flags = QUIC_CREDENTIAL_FLAG_NONE
-
-        // Load certificate configuration
+        // TODO: load certificate configuration
         // Add your certificate loading logic here
 
         let buffer = Data("sample".utf8)
