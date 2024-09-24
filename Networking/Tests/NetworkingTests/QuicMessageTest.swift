af53deee5e3b554fca8594aff9509996d55ed234
MacOMNI <414294494@qq.com>
2024-09-24 10:56:40 +0800

    update quic more test

--- Networking/Tests/NetworkingTests/QuicMessageTest.swift
+++ Networking/Tests/NetworkingTests/QuicMessageTest.swift
@@ -3,13 +3,6 @@ import Foundation
 import Testing
 
 struct QuicMessageTests {
-    @Test func testEncodingAndDecodingDataMessage() throws {
-        let originalMessage = QuicMessage(type: .data, data: "Test data".data(using: .utf8))
-        let encodedData = try JSONEncoder().encode(originalMessage)
-        let decodedMessage = try JSONDecoder().decode(QuicMessage.self, from: encodedData)
-        #expect(originalMessage == decodedMessage)
-    }
-
     @Test func testEncodingAndDecodingUnknownMessage() throws {
         let originalMessage = QuicMessage(type: .unknown, data: nil)
         let encodedData = try JSONEncoder().encode(originalMessage)
@@ -18,7 +11,7 @@ struct QuicMessageTests {
     }
 
     @Test func testEncodingAndDecodingReceivedMessage() throws {
-        let originalMessage = QuicMessage(type: .received, data: "Received data".data(using: .utf8))
+        let originalMessage = QuicMessage(type: .received, data: Data("received".utf8))
         let encodedData = try JSONEncoder().encode(originalMessage)
         let decodedMessage = try JSONDecoder().decode(QuicMessage.self, from: encodedData)
         #expect(originalMessage == decodedMessage)
