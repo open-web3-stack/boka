b4a4e25fb497bcb10d9e4a7c8e124cab8c7c3d03
MacOMNI <414294494@qq.com>
2024-09-25 11:15:22 +0800

    Update NetAddr.swift

--- Networking/Sources/Networking/msquic/NetAddr.swift
+++ Networking/Sources/Networking/msquic/NetAddr.swift
@@ -4,12 +4,6 @@ struct NetAddr: Hashable {
     var ipAddress: String
     var port: UInt16
 
-    // Implement the hash(into:) method
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(ipAddress)
-        hasher.combine(port)
-    }
-
     // Implement the == operator
     static func == (lhs: NetAddr, rhs: NetAddr) -> Bool {
         lhs.ipAddress == rhs.ipAddress && lhs.port == rhs.port
