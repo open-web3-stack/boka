1cd77b4ae3ae30cbd34fb518a4e006132656f985
Qiwei Yang <yangqiwei97@gmail.com>
2024-09-30 14:41:39 +0800

    basic saturating number

---
+++ Utils/Sources/Utils/SaturatingNumber.swift
@@ -0,0 +1,103 @@
+import Numerics
+
+public struct SaturatingNumber<T: FixedWidthInteger & Sendable>: Sendable {
+    public private(set) var value: T
+
+    public static var max: SaturatingNumber {
+        SaturatingNumber(T.max)
+    }
+
+    public static var min: SaturatingNumber {
+        SaturatingNumber(T.min)
+    }
+
+    public init(_ value: T) {
+        self.value = value
+    }
+
+    // Initializer for converting from other integer types to `T` with saturation
+    public init(_ value: some FixedWidthInteger & BinaryInteger) {
+        self.value = T(clamping: value)
+    }
+
+    public static func + (lhs: SaturatingNumber, rhs: SaturatingNumber) -> SaturatingNumber {
+        SaturatingNumber(lhs.value.addingWithSaturation(rhs.value))
+    }
+
+    public static func - (lhs: SaturatingNumber, rhs: SaturatingNumber) -> SaturatingNumber {
+        SaturatingNumber(lhs.value.subtractingWithSaturation(rhs.value))
+    }
+
+    public static func * (lhs: SaturatingNumber, rhs: SaturatingNumber) -> SaturatingNumber {
+        SaturatingNumber(lhs.value.multipliedWithSaturation(by: rhs.value))
+    }
+
+    public static prefix func - (lhs: SaturatingNumber) -> SaturatingNumber {
+        SaturatingNumber(lhs.value.negatedWithSaturation())
+    }
+
+    public static func += (lhs: inout SaturatingNumber, rhs: SaturatingNumber) {
+        lhs.value = lhs.value.addingWithSaturation(rhs.value)
+    }
+
+    public static func -= (lhs: inout SaturatingNumber, rhs: SaturatingNumber) {
+        lhs.value = lhs.value.subtractingWithSaturation(rhs.value)
+    }
+
+    public static func *= (lhs: inout SaturatingNumber, rhs: SaturatingNumber) {
+        lhs.value = lhs.value.multipliedWithSaturation(by: rhs.value)
+    }
+
+    // With other types
+
+    public static func + (lhs: SaturatingNumber, rhs: T) -> SaturatingNumber {
+        SaturatingNumber(lhs.value.addingWithSaturation(rhs))
+    }
+
+    public static func - (lhs: SaturatingNumber, rhs: T) -> SaturatingNumber {
+        SaturatingNumber(lhs.value.subtractingWithSaturation(rhs))
+    }
+
+    public static func * (lhs: SaturatingNumber, rhs: T) -> SaturatingNumber {
+        SaturatingNumber(lhs.value.multipliedWithSaturation(by: rhs))
+    }
+
+    public static func += (lhs: inout SaturatingNumber, rhs: T) {
+        lhs.value = lhs.value.addingWithSaturation(rhs)
+    }
+
+    public static func -= (lhs: inout SaturatingNumber, rhs: T) {
+        lhs.value = lhs.value.subtractingWithSaturation(rhs)
+    }
+
+    public static func *= (lhs: inout SaturatingNumber, rhs: T) {
+        lhs.value = lhs.value.multipliedWithSaturation(by: rhs)
+    }
+}
+
+extension SaturatingNumber: Equatable where T: Equatable {}
+
+extension SaturatingNumber: Comparable {
+    public static func < (lhs: SaturatingNumber, rhs: SaturatingNumber) -> Bool {
+        lhs.value < rhs.value
+    }
+}
+
+extension SaturatingNumber: CustomStringConvertible {
+    public var description: String {
+        "\(value)"
+    }
+}
+
+// TODO: check if this is correct
+extension SaturatingNumber: Codable where T: Codable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        value = try container.decode(T.self)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(value)
+    }
+}
---
+++ Utils/Sources/Utils/primitives.swift
@@ -0,0 +1,3 @@
+public typealias Gas = SaturatingNumber<UInt64>
+public typealias GasInt = SaturatingNumber<Int64>
+public typealias Balance = SaturatingNumber<UInt64>
