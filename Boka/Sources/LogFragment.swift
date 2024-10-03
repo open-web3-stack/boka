b0631ac36379c465935fba03816d88799a55a15c
Xiliang Chen <xlchen1291@gmail.com>
2024-10-03 21:18:29 +1300

    fix log filtering (#142)

--- Boka/Sources/LogFragment.swift
+++ Boka/Sources/LogFragment.swift
@@ -43,13 +43,9 @@ private struct InnerFragment: LoggerFragment {
 }
 
 public final class LogFragment: LoggerFragment {
-    private let defaultLevel: Logger.Level
-    private let filters: ThreadSafeContainer<[String: Logger.Level]>
     private let inner: LoggerFragment
 
-    public init(defaultLevel: Logger.Level = .info, filters: [String: Logger.Level] = [:]) {
-        self.defaultLevel = defaultLevel
-        self.filters = .init(filters)
+    public init() {
         inner = TimestampFragment()
             .and(InnerFragment().separated(" "))
             .and(MessageFragment().separated(" "))
@@ -57,57 +53,11 @@ public final class LogFragment: LoggerFragment {
             .and(SourceLocationFragment().separated(" ").maxLevel(.debug))
     }
 
-    public func levelFor(label: String) -> Logger.Level {
-        let level: Logger.Level? = filters.read { filters in filters[label] }
-        if let level {
-            return level
-        }
-
-        let defaultLevel = defaultLevel
-        return filters.mutate { filters in
-            for (key, value) in filters where label.hasPrefix(key) {
-                filters[label] = value
-                return value
-            }
-            filters[label] = defaultLevel
-            return defaultLevel
-        }
-    }
-
     public func hasContent(record: inout LogRecord) -> Bool {
-        let level = levelFor(label: record.label)
-        return record.level > level
+        inner.hasContent(record: &record)
     }
 
     public func write(_ record: inout LogRecord, to: inout FragmentOutput) {
-        let level = levelFor(label: record.label)
-        if record.level < level {
-            return
-        }
         inner.write(&record, to: &to)
-    }
-
-    public static func parse(from: String) -> (LogFragment, Logger.Level)? {
-        var defaultLevel: Logger.Level?
-        var lowestLevel = Logger.Level.critical
-        var filters: [String: Logger.Level] = [:]
-        let parts = from.split(separator: ",")
-        for part in parts {
-            let entry = part.split(separator: "=")
-            switch entry.count {
-            case 1:
-                defaultLevel = Logger.Level(String(entry[0]))
-            case 2:
-                guard let level = Logger.Level(String(entry[1])) else {
-                    return nil
-                }
-                filters[String(entry[0])] = level
-                lowestLevel = min(lowestLevel, level)
-            default:
-                return nil
-            }
-        }
-
-        return (Self(defaultLevel: defaultLevel ?? .info, filters: filters), min(lowestLevel, defaultLevel ?? .critical))
     }
 }
--- Boka/Sources/Tracing.swift
+++ Boka/Sources/Tracing.swift
@@ -5,21 +5,73 @@ import OTLPGRPC
 import ServiceLifecycle
 import TracingUtils
 
+public func parse(from: String) -> (
+    filters: [String: Logger.Level],
+    defaultLevel: Logger.Level,
+    minimalLevel: Logger.Level
+)? {
+    var defaultLevel: Logger.Level?
+    var lowestLevel = Logger.Level.critical
+    var filters: [String: Logger.Level] = [:]
+    let parts = from.split(separator: ",")
+    for part in parts {
+        let entry = part.split(separator: "=")
+        switch entry.count {
+        case 1:
+            guard let level = parseLevel(String(entry[0])) else {
+                return nil
+            }
+            defaultLevel = level
+        case 2:
+            guard let level = parseLevel(String(entry[1])) else {
+                return nil
+            }
+            filters[String(entry[0].lowercased())] = level
+            lowestLevel = min(lowestLevel, level)
+        default:
+            return nil
+        }
+    }
+
+    return (
+        filters: filters,
+        defaultLevel: defaultLevel ?? .info,
+        minimalLevel: min(lowestLevel, defaultLevel ?? .critical)
+    )
+}
+
+private func parseLevel(_ level: String) -> Logger.Level? {
+    switch level.lowercased().trimmingCharacters(in: .whitespaces) {
+    case "trace": .trace
+    case "debug": .debug
+    case "info": .info
+    case "notice": .notice
+    case "warn", "warning": .warning
+    case "error": .error
+    case "critical": .critical
+    default: nil
+    }
+}
+
 public enum Tracing {
     public static func bootstrap(_ serviceName: String, loggerOnly: Bool = false) async throws -> [Service] {
         let env = ProcessInfo.processInfo.environment
-        let (logFragment, level) = LogFragment.parse(from: env["LOG_LEVEL"] ?? "") ?? {
+
+        let (filters, defaultLevel, minimalLevel) = parse(from: env["LOG_LEVEL"] ?? "") ?? {
             print("Invalid LOG_LEVEL, using default")
-            return (LogFragment(defaultLevel: .info), .info)
+            return (filters: [:], defaultLevel: .info, minimalLevel: .info)
         }()
 
-        // Bootstrap the logging backend with the OTel metadata provider which includes span IDs in logging messages.
-        LoggingSystem.bootstrap(
-            fragment: logFragment,
-            console: Terminal(),
-            level: level,
-            metadataProvider: .otel
-        )
+        LoggingSystem.bootstrap({ label, metadataProvider in
+            BokaLogger(
+                fragment: LogFragment(),
+                label: label,
+                level: minimalLevel,
+                metadataProvider: metadataProvider,
+                defaultLevel: defaultLevel,
+                filters: filters
+            )
+        }, metadataProvider: .otel)
 
         if loggerOnly {
             return []
