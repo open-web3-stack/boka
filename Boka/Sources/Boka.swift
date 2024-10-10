13034d8b5e1a2c545f258ad32e49392a6ac2522d
MacOMNI <414294494@qq.com>
2024-10-10 16:53:15 +0800

    update cli

--- Boka/Sources/Boka.swift
+++ Boka/Sources/Boka.swift
@@ -6,6 +6,7 @@
 
 import ArgumentParser
 import Blockchain
+import Foundation
 import Node
 import ServiceLifecycle
 import TracingUtils
@@ -13,13 +14,80 @@ import Utils
 
 @main
 struct Boka: AsyncParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "A command-line tool for Boka.",
+        version: "1.0.0"
+    )
+
+    @Option(name: [.customShort("d"), .long], help: "Base path to database files.")
+    var basePath: String?
+
+    @Option(name: [.customShort("c"), .long], help: "Path to chain spec file.")
+    var chain: String?
+
+    @Option(name: [.customShort("f"), .long], help: "Path to config file.")
+    var configFile: String?
+
+    @Option(
+        name: [.customLong("rpc"), .long],
+        help:
+        "Listen address for RPC server. Pass 'false' to disable RPC server. Default to 127.0.0.1:9955."
+    )
+    var rpcListenAddress: String = "127.0.0.1:9955"
+
+    @Option(name: [.customLong("p2p"), .long], help: "Listen address for P2P protocol.")
+    var p2pListenAddress: String?
+
+    @Option(name: [.customLong("peers"), .long], parsing: .upToNextOption, help: "Specify peer P2P addresses.")
+    var p2pPeers: [String] = []
+
+    @Flag(name: .long, help: "Run as a validator.")
+    var validator: Bool = false
+
+    @Option(
+        name: [.customLong("operator-rpc"), .long],
+        help:
+        "Listen address for operator RPC server. Pass 'false' to disable operator RPC server. Default to false."
+    )
+    var operatorRpcListenAddress: String = "false"
+
+    @Option(name: .long, help: "For development only. Seed for validator keys.")
+    var devSeed: String?
+
     mutating func run() async throws {
-        let services = try await Tracing.bootstrap("Boka", loggerOnly: true)
         let logger = Logger(label: "boka")
+
+        if let basePath {
+            logger.info("Base Path: \(basePath)")
+        }
+        if let chain {
+            logger.info("Chain: \(chain)")
+        }
+        if let configFile {
+            logger.info("Config File: \(configFile)")
+        }
+
+        logger.info("RPC Listen Address: \(rpcListenAddress)")
+
+        if let p2pListenAddress {
+            logger.info("P2P Listen Address: \(p2pListenAddress)")
+        }
+        if !p2pPeers.isEmpty {
+            logger.info("P2P Peers: \(p2pPeers.joined(separator: ", "))")
+        }
+        logger.info("Validator: \(validator ? "Enabled" : "Disabled")")
+        logger.info("Operator RPC Listen Address: \(operatorRpcListenAddress)")
+        if let devSeed {
+            logger.info("Dev Seed: \(devSeed)")
+        }
+
+        let services = try await Tracing.bootstrap("Boka", loggerOnly: true)
 
         logger.info("Starting Boka...")
 
-        let config = Node.Config(rpc: RPCConfig(listenAddress: "127.0.0.1", port: 9955))
+        let config = Node.Config(
+            rpc: RPCConfig(listenAddress: "127.0.0.1", port: 9955)
+        )
         let eventBus = EventBus(
             eventMiddleware: .serial(
                 .log(logger: Logger(label: "EventBus")),
@@ -29,7 +97,9 @@ struct Boka: AsyncParsableCommand {
         )
         do {
             let keystore = try await DevKeyStore()
-            let node = try await ValidatorNode(genesis: .dev, config: config, eventBus: eventBus, keystore: keystore)
+            let node = try await ValidatorNode(
+                genesis: .dev, config: config, eventBus: eventBus, keystore: keystore
+            )
 
             for service in services {
                 Task {
