f899bcdf57e571e6036a9bc01f012de5f488cf4f
MacOMNI <414294494@qq.com>
2024-10-15 07:36:53 +0800

    update genesis

--- Node/Sources/Node/Genesis.swift
+++ Node/Sources/Node/Genesis.swift
@@ -10,15 +10,12 @@ public enum Genesis {
 public enum GenesisError: Error {
     case invalidFormat(String)
     case fileReadError(Error)
-    case decodingError(Error)
     var errorDescription: String? {
         switch self {
         case let .invalidFormat(message):
             message
         case let .fileReadError(error):
             "File read error: \(error.localizedDescription)"
-        case let .decodingError(error):
-            "Decoding error: \(error.localizedDescription)"
         }
     }
 }
@@ -31,7 +28,7 @@ extension Genesis {
             let state = try State.devGenesis(config: config)
             return (StateRef(state), config)
         case let .file(path):
-            let genesis = try GenesisFileHandler().readAndValidateGenesis(from: path)
+            let genesis = try readAndValidateGenesis(from: path)
             var config: ProtocolConfig
             let preset = genesis.preset?.lowercased()
             switch preset {
@@ -45,6 +42,44 @@ extension Genesis {
             }
             let state = try State.devGenesis(config: Ref(config))
             return (StateRef(state), Ref(config))
+        }
+    }
+
+    private func validate(_ genesis: GenesisData) throws {
+        // Validate required fields
+        if genesis.name.isEmpty {
+            throw GenesisError.invalidFormat("Invalid or missing 'name'")
+        }
+        if genesis.id.isEmpty {
+            throw GenesisError.invalidFormat("Invalid or missing 'id'")
+        }
+        if genesis.bootnodes.isEmpty {
+            throw GenesisError.invalidFormat("Invalid or missing 'bootnodes'")
+        }
+        if genesis.state.isEmpty {
+            throw GenesisError.invalidFormat("Invalid or missing 'state'")
+        }
+        let preset = genesis.preset?.lowercased()
+        if preset != nil, !["dev", "mainnet"].contains(preset!) {
+            throw GenesisError.invalidFormat("Invalid preset value. Must be 'dev' or 'mainnet'.")
+        }
+        if preset == nil, genesis.validateConfig() {
+            throw GenesisError.invalidFormat("Missing 'preset' or 'config' field.")
+        }
+    }
+
+    func readAndValidateGenesis(from filePath: String) throws -> GenesisData {
+        do {
+            let fileContents = try String(contentsOfFile: filePath, encoding: .utf8)
+            let data = fileContents.data(using: .utf8)!
+            let decoder = JSONDecoder()
+            let genesis = try decoder.decode(GenesisData.self, from: data)
+            try validate(genesis)
+            return genesis
+        } catch let error as GenesisError {
+            throw error
+        } catch {
+            throw GenesisError.fileReadError(error)
         }
     }
 }
@@ -68,8 +103,8 @@ struct GenesisData: Sendable, Codable {
         state = try container.decode(String.self, forKey: .state)
     }
 
-    func missConfigFiled() -> Bool {
-        guard let config else {
+    func validateConfig() -> Bool {
+        guard let config: ProtocolConfig else {
             return true
         }
         let mirror = Mirror(reflecting: config)
@@ -82,46 +117,5 @@ struct GenesisData: Sendable, Codable {
             }
         }
         return false
-    }
-}
-
-// Class to handle Genesis JSON file operations
-class GenesisFileHandler {
-    private func validateGenesis(_ genesis: GenesisData) throws {
-        // Validate required fields
-        if genesis.name.isEmpty {
-            throw GenesisError.invalidFormat("Invalid or missing 'name'")
-        }
-        if genesis.id.isEmpty {
-            throw GenesisError.invalidFormat("Invalid or missing 'id'")
-        }
-        if genesis.bootnodes.isEmpty {
-            throw GenesisError.invalidFormat("Invalid or missing 'bootnodes'")
-        }
-        if genesis.state.isEmpty {
-            throw GenesisError.invalidFormat("Invalid or missing 'state'")
-        }
-        let preset = genesis.preset?.lowercased()
-        if preset != nil, !["dev", "mainnet"].contains(preset!) {
-            throw GenesisError.invalidFormat("Invalid preset value. Must be 'dev' or 'mainnet'.")
-        }
-        if preset == nil, genesis.missConfigFiled() {
-            throw GenesisError.invalidFormat("Missing 'preset' or 'config' field.")
-        }
-    }
-
-    func readAndValidateGenesis(from filePath: String) throws -> GenesisData {
-        do {
-            let fileContents = try String(contentsOfFile: filePath, encoding: .utf8)
-            let data = fileContents.data(using: .utf8)!
-            let decoder = JSONDecoder()
-            let genesis = try decoder.decode(GenesisData.self, from: data)
-            try validateGenesis(genesis)
-            return genesis
-        } catch let error as GenesisError {
-            throw error
-        } catch {
-            throw GenesisError.fileReadError(error)
-        }
     }
 }
