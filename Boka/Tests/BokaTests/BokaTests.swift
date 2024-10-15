76213edd75b67e5565b4976002d745532ad94c6d
MacOMNI <414294494@qq.com>
2024-10-15 19:51:00 +0800

    update boka test

--- Boka/Tests/BokaTests/BokaTests.swift
+++ Boka/Tests/BokaTests/BokaTests.swift
@@ -1,5 +1,7 @@
+import Blockchain
 import ConsoleKit
 import Foundation
+import Node
 import Testing
 
 @testable import Boka
@@ -30,9 +32,28 @@ final class BokaTests {
 
     @Test func commandWithAllConfig() async throws {
         let sepc = ResourceLoader.loadResource(named: "devnet_allconfig_spec.json")!.path()
-        print("path = \(sepc)")
-        let input = CommandInput(arguments: ["Boka", "-f", sepc])
-        try await console.run(boka, input: input)
+        let genesis: Genesis = .file(path: sepc)
+        let (_, protocolConfig) = try await genesis.load()
+        #expect(protocolConfig.value.maxWorkItems == 2)
+        #expect(protocolConfig.value.serviceMinBalance == 100)
+    }
+
+    @Test func commandWithSomeConfig() async throws {
+        let sepc = ResourceLoader.loadResource(named: "mainnet_someconfig_spec.json")!.path()
+        let genesis: Genesis = .file(path: sepc)
+        let config = ProtocolConfigRef.mainnet.value
+        let (_, protocolConfig) = try await genesis.load()
+        #expect(protocolConfig.value.auditTranchePeriod == 100)
+        #expect(protocolConfig.value.pvmProgramInitSegmentSize == config.pvmProgramInitSegmentSize)
+    }
+
+    @Test func commandWithNoConfig() async throws {
+        let sepc = ResourceLoader.loadResource(named: "devnet_noconfig_spec.json")!.path()
+        let genesis: Genesis = .file(path: sepc)
+        let config = ProtocolConfigRef.dev.value
+        let (_, protocolConfig) = try await genesis.load()
+        #expect(protocolConfig.value.maxWorkItems == config.maxWorkItems)
+        #expect(protocolConfig.value.serviceMinBalance == config.serviceMinBalance)
     }
 
     @Test func commandWithWrongFilePath() async throws {
