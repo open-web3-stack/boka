b4158c7a300e79bbaff197e7f570dc184bc75cdb
MacOMNI <414294494@qq.com>
2024-10-18 17:44:24 +0800

    update clang format

--- Networking/Sources/CHelpers/helpers.c
+++ Networking/Sources/CHelpers/helpers.c
@@ -8,8 +8,13 @@
 #include "helpers.h"
 
 // Function to parse certificate and extract public key and alternative name
-int parse_pkcs12_certificate(const unsigned char *data, size_t length, unsigned char **public_key,
-                             size_t *public_key_len, char **alt_name, char **error_message)
+int parse_pkcs12_certificate(
+    const unsigned char *data,
+    size_t length,
+    unsigned char **public_key,
+    size_t *public_key_len,
+    char **alt_name,
+    char **error_message)
 {
     int ret = -1;
     BIO *bio = NULL;
@@ -20,60 +25,50 @@ int parse_pkcs12_certificate(const unsigned char *data, size_t length, unsigned 
     STACK_OF(GENERAL_NAME) *alt_names = NULL;
 
     bio = BIO_new_mem_buf(data, (int)length);
-    if (!bio)
-    {
+    if (!bio) {
         *error_message = strdup("Failed to create BIO.");
         goto cleanup;
     }
     p12 = d2i_PKCS12_bio(bio, NULL);
-    if (!p12)
-    {
+    if (!p12) {
         *error_message = strdup("Failed to parse PKCS12.");
         goto cleanup;
     }
 
-    if (!PKCS12_parse(p12, NULL, &pkey, &cert, &ca))
-    {
+    if (!PKCS12_parse(p12, NULL, &pkey, &cert, &ca)) {
         *error_message = strdup("Failed to parse PKCS12 structure.");
         goto cleanup;
     }
 
     // Check if the key is ED25519
     int sig_alg = X509_get_signature_nid(cert);
-    if (sig_alg != NID_ED25519)
-    {
+    if (sig_alg != NID_ED25519) {
         *error_message = strdup("Certificate signature algorithm is not ED25519.");
         goto cleanup;
     }
 
     // Extract public key
-    if (EVP_PKEY_get_raw_public_key(pkey, NULL, public_key_len) <= 0)
-    {
+    if (EVP_PKEY_get_raw_public_key(pkey, NULL, public_key_len) <= 0) {
         *error_message = strdup("Failed to get public key length.");
         goto cleanup;
     }
     *public_key = (unsigned char *)malloc(*public_key_len);
-    if (!*public_key)
-    {
+    if (!*public_key) {
         *error_message = strdup("Failed to allocate memory for public key.");
         goto cleanup;
     }
 
-    if (EVP_PKEY_get_raw_public_key(pkey, *public_key, public_key_len) <= 0)
-    {
+    if (EVP_PKEY_get_raw_public_key(pkey, *public_key, public_key_len) <= 0) {
         *error_message = strdup("Failed to extract public key.");
         goto cleanup;
     }
 
     // Extract alternative name
     alt_names = X509_get_ext_d2i(cert, NID_subject_alt_name, NULL, NULL);
-    if (alt_names)
-    {
-        for (int i = 0; i < sk_GENERAL_NAME_num(alt_names); i++)
-        {
+    if (alt_names) {
+        for (int i = 0; i < sk_GENERAL_NAME_num(alt_names); i++) {
             GENERAL_NAME *gen_name = sk_GENERAL_NAME_value(alt_names, i);
-            if (gen_name->type == GEN_DNS)
-            {
+            if (gen_name->type == GEN_DNS) {
                 ASN1_STRING *name = gen_name->d.dNSName;
                 *alt_name = strdup((char *)ASN1_STRING_get0_data(name));
                 break;
@@ -82,8 +77,7 @@ int parse_pkcs12_certificate(const unsigned char *data, size_t length, unsigned 
         sk_GENERAL_NAME_pop_free(alt_names, GENERAL_NAME_free);
     }
 
-    if (!*alt_name)
-    {
+    if (!*alt_name) {
         *error_message = strdup("No alternative name found.");
         goto cleanup;
     }
@@ -91,8 +85,7 @@ int parse_pkcs12_certificate(const unsigned char *data, size_t length, unsigned 
     ret = EXIT_SUCCESS; // Success
 
 cleanup:
-    if (ret != 0 && *public_key)
-    {
+    if (ret != 0 && *public_key) {
         free(*public_key);
         *public_key = NULL;
     }
@@ -105,10 +98,12 @@ cleanup:
     return ret;
 }
 
-int generate_self_signed_cert_and_pkcs12(const unsigned char *private_key_buf,
-                                         size_t private_key_len,
-                                         const char *alt_name, // null terminated string
-                                         unsigned char **pkcs12_data, int *pkcs12_len)
+int generate_self_signed_cert_and_pkcs12(
+    const unsigned char *private_key_buf,
+    size_t private_key_len,
+    const char *alt_name, // null terminated string
+    unsigned char **pkcs12_data,
+    int *pkcs12_len)
 {
     X509 *cert = NULL;
     PKCS12 *p12 = NULL;
@@ -118,15 +113,13 @@ int generate_self_signed_cert_and_pkcs12(const unsigned char *private_key_buf,
     // Create EVP_PKEY from the provided Ed25519 private key buffer
     ed25519_key =
         EVP_PKEY_new_raw_private_key(EVP_PKEY_ED25519, NULL, private_key_buf, private_key_len);
-    if (!ed25519_key)
-    {
+    if (!ed25519_key) {
         goto cleanup;
     }
 
     // Create a new X509 certificate
     cert = X509_new();
-    if (!cert)
-    {
+    if (!cert) {
         goto cleanup;
     }
 
@@ -142,8 +135,8 @@ int generate_self_signed_cert_and_pkcs12(const unsigned char *private_key_buf,
 
     // Set subject and issuer (self-signed, so they're the same)
     X509_NAME *name = X509_get_subject_name(cert);
-    X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC, (unsigned char *)"Self-Signed Cert", -1,
-                               -1, 0);
+    X509_NAME_add_entry_by_txt(
+        name, "CN", MBSTRING_ASC, (unsigned char *)"Self-Signed Cert", -1, -1, 0);
     X509_set_issuer_name(cert, name);
 
     // Set public key
@@ -160,29 +153,25 @@ int generate_self_signed_cert_and_pkcs12(const unsigned char *private_key_buf,
     GENERAL_NAMES_free(alt_names);
 
     // Self-sign the certificate
-    if (!X509_sign(cert, ed25519_key, NULL))
-    {
+    if (!X509_sign(cert, ed25519_key, NULL)) {
         goto cleanup;
     }
 
     // Create PKCS12 structure
     p12 = PKCS12_create(NULL, "My Certificate", ed25519_key, cert, NULL, 0, 0, 0, 0, 0);
-    if (!p12)
-    {
+    if (!p12) {
         goto cleanup;
     }
 
     // Get the size of the DER-encoded PKCS12 structure
     *pkcs12_len = i2d_PKCS12(p12, NULL);
-    if (*pkcs12_len <= 0)
-    {
+    if (*pkcs12_len <= 0) {
         goto cleanup;
     }
 
     // Allocate memory for the DER-encoded PKCS12 structure
     *pkcs12_data = malloc(*pkcs12_len);
-    if (!*pkcs12_data)
-    {
+    if (!*pkcs12_data) {
         goto cleanup;
     }
 
@@ -191,8 +180,7 @@ int generate_self_signed_cert_and_pkcs12(const unsigned char *private_key_buf,
 
     // Actually encode the PKCS12 structure
     *pkcs12_len = i2d_PKCS12(p12, &temp_buf);
-    if (*pkcs12_len <= 0)
-    {
+    if (*pkcs12_len <= 0) {
         ret = 6;
         free(*pkcs12_data);
         *pkcs12_data = NULL;
@@ -203,8 +191,7 @@ int generate_self_signed_cert_and_pkcs12(const unsigned char *private_key_buf,
     ret = 0;
 
 cleanup:
-    if (ret != 0)
-    {
+    if (ret != 0) {
         ret = ERR_get_error();
     }
     X509_free(cert);
@@ -213,4 +200,7 @@ cleanup:
     return ret;
 }
 
-char *get_error_string(int error) { return ERR_error_string(error, NULL); }
+char *get_error_string(int error)
+{
+    return ERR_error_string(error, NULL);
+}
