ba4d3ed2f438f36db19492020c46d833f3ae8d63
MacOMNI <414294494@qq.com>
2024-11-05 10:30:26 +0800

    update

--- Networking/Sources/Networking/Peer.swift
+++ Networking/Sources/Networking/Peer.swift
@@ -440,7 +440,7 @@ private struct PeerEventHandler<Handler: StreamHandler>: QuicEventHandler {
         }
         // Check if the connection is already reconnected
         impl.reconnectStates.write { reconnectStates in
-            if let _ = reconnectStates[conn.remoteAddress] {
+            if reconnectStates[conn.remoteAddress] != nil {
                 reconnectStates[conn.remoteAddress] = nil
             }
         }
