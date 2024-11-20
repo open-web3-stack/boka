bf963f8276304d3ad7e7374289c08ec40bc5c4f7
MacOMNI <414294494@qq.com>
2024-11-20 12:01:10 +0800

    update backpressure

--- Networking/Tests/NetworkingTests/MockPeerEventTests.swift
+++ Networking/Tests/NetworkingTests/MockPeerEventTests.swift
@@ -31,15 +31,16 @@ final class MockPeerEventTests {
         }
 
         private func backpressure(_ data: Data, on stream: QuicStream) {
+            logger.info("backpressure for stream: \(stream.id)")
             let state = resendStates.read { reconnectStates in
                 reconnectStates[stream.id] ?? .init()
             }
 
             do {
                 let windowSize = try stream.getFlowControlWindow()
-
+                logger.info("backpressure: \(stream.id) window size: \(windowSize)")
                 guard state.attempt < maxRetryAttempts else {
-                    logger.warning("resend: \(stream.id) reached max retry attempts")
+                    logger.warning("backpressure: \(stream.id) reached max retry attempts")
                     try? stream.adjustFlowControl(recvBufferSize: Int(Int16.max))
                     return
                 }
@@ -159,6 +160,65 @@ final class MockPeerEventTests {
     }
 
     @Test
+    func backpressureRetriesWithinLimit() async throws {
+        let serverHandler = MockPeerEventHandler(true)
+        let clientHandler = MockPeerEventHandler(true)
+        let privateKey1 = try Ed25519.SecretKey(from: Data32.random())
+        let cert = try generateSelfSignedCertificate(privateKey: privateKey1)
+        let serverConfiguration = try QuicConfiguration(
+            registration: registration,
+            pkcs12: certData,
+            alpns: [Data("testalpn".utf8)],
+            client: false,
+            settings: QuicSettings.defaultSettings
+        )
+
+        let listener = try QuicListener(
+            handler: serverHandler,
+            registration: registration,
+            configuration: serverConfiguration,
+            listenAddress: NetAddr(ipAddress: "127.0.0.1", port: 0)!,
+            alpns: [Data("testalpn".utf8)]
+        )
+
+        let listenAddress = try listener.listenAddress()
+        // Client setup with certificate
+        let clientConfiguration = try QuicConfiguration(
+            registration: registration,
+            pkcs12: cert,
+            alpns: [Data("testalpn".utf8)],
+            client: true,
+            settings: QuicSettings.defaultSettings
+        )
+
+        let clientConnection = try QuicConnection(
+            handler: clientHandler,
+            registration: registration,
+            configuration: clientConfiguration
+        )
+        // Attempt to connect
+        try clientConnection.connect(to: listenAddress)
+        let stream1 = try clientConnection.createStream()
+        try stream1.send(data: Data("test data 1".utf8))
+
+        try await Task.sleep(for: .milliseconds(100))
+        let (_, info) = serverHandler.events.value.compactMap {
+            switch $0 {
+            case let .newConnection(_, connection, info):
+                (connection, info) as (QuicConnection, ConnectionInfo)?
+            default:
+                nil
+            }
+        }.first!
+        let (ipAddress2, _) = info.remoteAddress.getAddressAndPort()
+
+        #expect(info.negotiatedAlpn == Data("testalpn".utf8))
+        #expect(info.serverName == "127.0.0.1")
+        #expect(info.localAddress == listenAddress)
+        #expect(ipAddress2 == "127.0.0.1")
+    }
+
+    @Test
     func rejectsConDueToBadServerCert() async throws {
         let serverHandler = MockPeerEventHandler()
         let clientHandler = MockPeerEventHandler()
@@ -219,7 +279,7 @@ final class MockPeerEventTests {
     func connected() async throws {
         let serverHandler = MockPeerEventHandler()
         let clientHandler = MockPeerEventHandler()
-        let privateKey1 = try Ed25519.SecretKey(from: Data32())
+        let privateKey1 = try Ed25519.SecretKey(from: Data32.random())
         let cert = try generateSelfSignedCertificate(privateKey: privateKey1)
         let serverConfiguration = try QuicConfiguration(
             registration: registration,
