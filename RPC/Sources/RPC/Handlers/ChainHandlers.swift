7aa80791c4b36718da2647717724cb67cb4b1087
Bryan Chen <xlchen1291@gmail.com>
2024-12-02 21:15:50 +1300

    add rpc methods

--- RPC/Sources/RPC/Handlers/ChainHandlers.swift
+++ RPC/Sources/RPC/Handlers/ChainHandlers.swift
@@ -1,21 +1,28 @@
 import Blockchain
+import Codec
 import Foundation
 import Utils
 
 public enum ChainHandlers {
     public static let handlers: [any RPCHandler.Type] = [
         GetBlock.self,
+        GetBlockHash.self,
+        GetFinalziedHead.self,
+        GetHeader.self,
     ]
 
     public static func getHandlers(source: ChainDataSource) -> [any RPCHandler] {
         [
             GetBlock(source: source),
+            GetBlockHash(source: source),
+            GetFinalziedHead(source: source),
+            GetHeader(source: source),
         ]
     }
 
     public struct GetBlock: RPCHandler {
         public typealias Request = Request1<Data32?>
-        public typealias Response = BlockRef?
+        public typealias Response = Data?
         public typealias DataSource = ChainDataSource
 
         public static var method: String { "chain_getBlock" }
@@ -28,11 +35,72 @@ public enum ChainHandlers {
         }
 
         public func handle(request: Request) async throws -> Response? {
-            if let hash = request.value {
+            let block = if let hash = request.value {
                 try await source.getBlock(hash: hash)
             } else {
                 try await source.getBestBlock()
             }
+            return try block.map { try JamEncoder.encode($0) }
+        }
+    }
+
+    public struct GetBlockHash: RPCHandler {
+        public typealias Request = Request1<TimeslotIndex?>
+        public typealias Response = Data?
+        public typealias DataSource = ChainDataSource
+
+        public static var method: String { "chain_getBlockHash" }
+        public static var summary: String? { "Get the block hash by timeslot. If timeslot is not provided, returns the best block hash." }
+
+        private let source: ChainDataSource
+
+        init(source: ChainDataSource) {
+            self.source = source
+        }
+
+        public func handle(request _: Request) async throws -> Response? {
+            // TODO: implement
+            nil
+        }
+    }
+
+    public struct GetFinalziedHead: RPCHandler {
+        public typealias Request = VoidRequest
+        public typealias Response = Data32?
+        public typealias DataSource = ChainDataSource
+
+        public static var method: String { "chain_getFinalziedHead" }
+        public static var summary: String? { "Get hash of the last finalized block in the canon chain." }
+
+        private let source: ChainDataSource
+
+        init(source: ChainDataSource) {
+            self.source = source
+        }
+
+        public func handle(request _: Request) async throws -> Response? {
+            // TODO: implement
+            nil
+        }
+    }
+
+    public struct GetHeader: RPCHandler {
+        public typealias Request = Request1<Data32?>
+        public typealias Response = Data?
+        public typealias DataSource = ChainDataSource
+
+        public static var method: String { "chain_getHeader" }
+        public static var summary: String? { "Get block header by hash. If hash is not provided, returns the best block header." }
+
+        private let source: ChainDataSource
+
+        init(source: ChainDataSource) {
+            self.source = source
+        }
+
+        public func handle(request _: Request) async throws -> Response? {
+            // TODO: implement
+            nil
         }
     }
 }
