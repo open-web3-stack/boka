a54168a2702a3cfa2a5c3dbb86c3eec2b296ebfe
Xiliang Chen <xlchen1291@gmail.com>
2024-12-04 20:14:34 +1300

    minor refactor for RocksDB (#244)
    * minor refactor
    
    * fix

--- Utils/Sources/Utils/SafePointer.swift
+++ Utils/Sources/Utils/SafePointer.swift
@@ -1,5 +1,13 @@
 public struct SafePointer: ~Copyable, Sendable {
-    let ptr: SendableOpaquePointer
-    let free: @Sendable (_ ptr: OpaquePointer) -> Void
+    public let ptr: SendableOpaquePointer
+    private let free: @Sendable (_ ptr: OpaquePointer) -> Void
+
+    public var value: OpaquePointer { ptr.value }
+
+    public init(ptr: OpaquePointer, free: @Sendable @escaping (OpaquePointer) -> Void) {
+        self.ptr = ptr.asSendable
+        self.free = free
+    }
+
     deinit { free(ptr.value) }
 }
