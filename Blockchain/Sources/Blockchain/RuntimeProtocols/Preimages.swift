eeb2ab4d5046fe47f8ed372b2d88ed89a5e23686
Xiliang Chen <xlchen1291@gmail.com>
2025-01-07 13:27:53 +1300

    Preimages tests (#263)
    * preimages
    
    * preimages test

---
+++ Blockchain/Sources/Blockchain/RuntimeProtocols/Preimages.swift
@@ -0,0 +1,78 @@
+import Foundation
+import Utils
+
+public enum PreimagesError: Error {
+    case preimagesNotSorted
+    case duplicatedPreimage
+    case invalidServiceIndex
+}
+
+public struct PreimageUpdate: Sendable, Equatable {
+    public let serviceIndex: UInt32
+    public let hash: Data32
+    public let data: Data
+    public let length: UInt32
+    public let timeslot: TimeslotIndex
+
+    public init(serviceIndex: UInt32, hash: Data32, data: Data, length: UInt32, timeslot: TimeslotIndex) {
+        self.serviceIndex = serviceIndex
+        self.hash = hash
+        self.data = data
+        self.length = length
+        self.timeslot = timeslot
+    }
+}
+
+public struct PreimagesPostState: Sendable, Equatable {
+    public let updates: [PreimageUpdate]
+
+    public init(updates: [PreimageUpdate]) {
+        self.updates = updates
+    }
+}
+
+public protocol Preimages {
+    func get(serviceAccount index: ServiceIndex, preimageHash hash: Data32) async throws -> Data?
+    func get(serviceAccount index: ServiceIndex, preimageHash hash: Data32, length: UInt32) async throws
+        -> LimitedSizeArray<TimeslotIndex, ConstInt0, ConstInt3>?
+
+    mutating func mergeWith(postState: PreimagesPostState)
+}
+
+extension Preimages {
+    public func updatePreimages(
+        config _: ProtocolConfigRef,
+        timeslot: TimeslotIndex,
+        preimages: ExtrinsicPreimages
+    ) async throws(PreimagesError) -> PreimagesPostState {
+        let preimages = preimages.preimages
+        var updates: [PreimageUpdate] = []
+
+        guard preimages.isSortedAndUnique() else {
+            throw PreimagesError.preimagesNotSorted
+        }
+
+        for preimage in preimages {
+            let hash = preimage.data.blake2b256hash()
+
+            // check prior state
+            let prevPreimageData = try await Result {
+                try await get(serviceAccount: preimage.serviceIndex, preimageHash: hash)
+            }.mapError { _ in PreimagesError.invalidServiceIndex }.get()
+
+            guard prevPreimageData == nil else {
+                throw PreimagesError.duplicatedPreimage
+            }
+
+            updates.append(PreimageUpdate(
+                serviceIndex: preimage.serviceIndex,
+                hash: hash,
+                data: preimage.data,
+                length: UInt32(preimage.data.count),
+                timeslot: timeslot
+            ))
+        }
+
+        return PreimagesPostState(updates: updates)
+    }
+}
--- Blockchain/Sources/Blockchain/Types/HashAndLength.swift
+++ Blockchain/Sources/Blockchain/Types/HashAndLength.swift
@@ -1,12 +1,19 @@
 import Utils
 
-public struct HashAndLength: Sendable, Codable {
+public struct HashAndLength: Sendable, Codable, Comparable {
     public var hash: Data32
     public var length: DataLength
 
     public init(hash: Data32, length: DataLength) {
         self.hash = hash
         self.length = length
+    }
+
+    public static func < (lhs: HashAndLength, rhs: HashAndLength) -> Bool {
+        if lhs.hash == rhs.hash {
+            return lhs.length < rhs.length
+        }
+        return lhs.hash < rhs.hash
     }
 }
 
@@ -16,6 +23,6 @@ extension HashAndLength: Hashable {
         // and we know the output is 32 bytes
         // so we can just take the first 4 bytes and should be good enough
         // NOTE: we will never use the Hashable protocol for any critical operations
-        hasher.combine(hash.data[0 ..< 4])
+        hasher.combine(hash.data[hash.data.startIndex ..< hash.data.startIndex + 4])
     }
 }
