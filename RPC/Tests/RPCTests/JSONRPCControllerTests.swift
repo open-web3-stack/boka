760b6e3abe31f866db851ff94978c1de11431f97
MacOMNI <414294494@qq.com>
2025-01-20 11:09:42 +0800

    add more rpc test

--- RPC/Tests/RPCTests/JSONRPCControllerTests.swift
+++ RPC/Tests/RPCTests/JSONRPCControllerTests.swift
@@ -2,6 +2,7 @@ import Blockchain
 @testable import RPC
 import Testing
 import TracingUtils
+@testable import Utils
 import Vapor
 import XCTVapor
 
@@ -28,7 +29,62 @@ final class JSONRPCControllerTests {
         try app.test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res in
             #expect(res.status == .ok)
             let resp = try res.content.decode(JSONResponse.self, using: JSONDecoder())
+            #expect((resp.result!.value as! Utils.JSON).bool == true)
+        }
+    }
+
+    @Test func implementation() throws {
+        let req = JSONRequest(jsonrpc: "2.0", method: "system_implementation", params: nil, id: 2)
+        var buffer = ByteBuffer()
+        try buffer.writeJSONEncodable(req)
+        try app.test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res in
+            #expect(res.status == .ok)
+            let resp = try res.content.decode(JSONResponse.self, using: JSONDecoder())
+            #expect((resp.result!.value as! Utils.JSON).string == "Boka")
+        }
+    }
+
+    @Test func version() throws {
+        let req = JSONRequest(jsonrpc: "2.0", method: "system_version", params: nil, id: 3)
+        var buffer = ByteBuffer()
+        try buffer.writeJSONEncodable(req)
+        try app.test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res in
+            #expect(res.status == .ok)
+            let resp = try res.content.decode(JSONResponse.self, using: JSONDecoder())
+            #expect((resp.result!.value as! Utils.JSON).string == "0.0.1")
+        }
+    }
+
+    @Test func properties() throws {
+        let req = JSONRequest(jsonrpc: "2.0", method: "system_properties", params: nil, id: 4)
+        var buffer = ByteBuffer()
+        try buffer.writeJSONEncodable(req)
+        try app.test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res in
+            #expect(res.status == .ok)
+            let resp = try res.content.decode(JSONResponse.self, using: JSONDecoder())
             #expect(resp.result?.value != nil)
+        }
+    }
+
+    @Test func nodeRoles() throws {
+        let req = JSONRequest(jsonrpc: "2.0", method: "system_nodeRoles", params: nil, id: 5)
+        var buffer = ByteBuffer()
+        try buffer.writeJSONEncodable(req)
+        try app.test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res in
+            #expect(res.status == .ok)
+            let resp = try res.content.decode(JSONResponse.self, using: JSONDecoder())
+            #expect((resp.result!.value as! Utils.JSON).array == [])
+        }
+    }
+
+    @Test func chain() throws {
+        let req = JSONRequest(jsonrpc: "2.0", method: "system_chain", params: nil, id: 6)
+        var buffer = ByteBuffer()
+        try buffer.writeJSONEncodable(req)
+        try app.test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res in
+            #expect(res.status == .ok)
+            let resp = try res.content.decode(JSONResponse.self, using: JSONDecoder())
+            #expect((resp.result!.value as! Utils.JSON).string == "dev")
         }
     }
 }
