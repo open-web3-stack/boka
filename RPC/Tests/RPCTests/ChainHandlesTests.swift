fcc40f3549a11cd767c370c98b703e37c4f5e865
MacOMNI <414294494@qq.com>
2025-01-21 17:49:10 +0800

    update chainhandles

--- RPC/Tests/RPCTests/ChainHandlesTests.swift
+++ RPC/Tests/RPCTests/ChainHandlesTests.swift
@@ -48,8 +48,6 @@ final class ChainRPCControllerTests {
         dataProvider = try! await BlockchainDataProvider(InMemoryDataProvider(genesisState: genesisState, genesisBlock: genesisBlock))
         let rpcController = JSONRPCController(handlers: ChainHandlers
             .getHandlers(source: DummyNodeDataSource(chainDataProvider: dataProvider)))
-//        let rpcController = JSONRPCController(handlers: SystemHandlers.getHandlers(source: DummySource()))
-
         try app.register(collection: rpcController)
     }
 
@@ -62,41 +60,46 @@ final class ChainRPCControllerTests {
         try buffer.writeJSONEncodable(req)
         try await app.test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res async in
             #expect(res.status == .ok)
-            print("resp \(res.body.string)")
         }
         try await app.asyncShutdown()
     }
-//
-//    @Test func getBlockHash() throws {
-//        let req = JSONRequest(jsonrpc: "2.0", method: "chain_getBlockHash", params: nil, id: 2)
-//        var buffer = ByteBuffer()
-//        try buffer.writeJSONEncodable(req)
-//        try app.test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res in
-//            #expect(res.status == .ok)
-//            let resp = try res.content.decode(JSONResponse.self, using: JSONDecoder())
-    ////            #expect((resp.result!.value as! Utils.JSON).data == "test_block_hash".data(using: .utf8))
-//        }
-//    }
-//
-//    @Test func getFinalizedHead() throws {
-//        let req = JSONRequest(jsonrpc: "2.0", method: "chain_getFinalziedHead", params: nil, id: 3)
-//        var buffer = ByteBuffer()
-//        try buffer.writeJSONEncodable(req)
-//        try app.test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res in
-//            #expect(res.status == .ok)
-//            let resp = try res.content.decode(JSONResponse.self, using: JSONDecoder())
-    ////            #expect((resp.result!.value as! Utils.JSON).data == "finalized_head_hash".data(using: .utf8))
-//        }
-//    }
-//
-//    @Test func getHeader() throws {
-//        let req = JSONRequest(jsonrpc: "2.0", method: "chain_getHeader", params: nil, id: 4)
-//        var buffer = ByteBuffer()
-//        try buffer.writeJSONEncodable(req)
-//        try app.test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res in
-//            #expect(res.status == .ok)
-//            let resp = try res.content.decode(JSONResponse.self, using: JSONDecoder())
-    ////            #expect((resp.result!.value as! Utils.JSON).data == "best_header_data".data(using: .utf8))
-//        }
-//    }
+
+    @Test func getBlockHash() async throws {
+        try await setUp()
+        let hashHex = await dataProvider.bestHead.hash.toHexString()
+        let params = JSON.array([.string(hashHex)])
+        let req = JSONRequest(jsonrpc: "2.0", method: "chain_getBlockHash", params: params, id: 1)
+        var buffer = ByteBuffer()
+        try buffer.writeJSONEncodable(req)
+        try await app.test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res async in
+            #expect(res.status == .ok)
+        }
+        try await app.asyncShutdown()
+    }
+
+    @Test func getFinalizedHead() async throws {
+        try await setUp()
+        let hashHex = await dataProvider.bestHead.hash.toHexString()
+        let params = JSON.array([.string(hashHex)])
+        let req = JSONRequest(jsonrpc: "2.0", method: "chain_getFinalziedHead", params: params, id: 1)
+        var buffer = ByteBuffer()
+        try buffer.writeJSONEncodable(req)
+        try await app.test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res async in
+            #expect(res.status == .ok)
+        }
+        try await app.asyncShutdown()
+    }
+
+    @Test func getHeader() async throws {
+        try await setUp()
+        let hashHex = await dataProvider.bestHead.hash.toHexString()
+        let params = JSON.array([.string(hashHex)])
+        let req = JSONRequest(jsonrpc: "2.0", method: "chain_getHeader", params: params, id: 1)
+        var buffer = ByteBuffer()
+        try buffer.writeJSONEncodable(req)
+        try await app.test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res async in
+            #expect(res.status == .ok)
+        }
+        try await app.asyncShutdown()
+    }
 }
