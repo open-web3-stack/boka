a0f317188ad8833e5bb0b215633d49e1e76afd67
Qiwei Yang <yangqiwei97@gmail.com>
2025-03-04 11:48:33 +0800

    community STF tests (#304)
    * add submodules
    
    * move files
    
    * setup
    
    * fix state merklize
    
    * decode tests
    
    * update submodule
    
    * update decode
    
    * fix decoder
    
    * pass safrole
    
    * fix block author

---
+++ JAMTests/Tests/JAMTests/jamtestnet/JamdunaTests.swift
@@ -0,0 +1,30 @@
+import Testing
+import Utils
+
+@testable import JAMTests
+
+struct JamdunaTests {
+    @Test(arguments: try JamTestnet.loadTests(path: "data/safrole/state_transitions", src: .jamduna))
+    func safroleTests(_ input: Testcase) async throws {
+        try await CommonTests.test(input)
+    }
+
+    @Test(arguments: try JamTestnet.loadTests(path: "data/fallback/state_transitions", src: .jamduna))
+    func fallbackTests(_ input: Testcase) async throws {
+        try await CommonTests.test(input)
+    }
+
+    // @Test(arguments: try JamTestnet.loadTests(path: "data/orderedaccumulation/state_transitions", src: .jamduna))
+    // func orderedaccumulationTests(_ input: Testcase) async throws {
+    //     await withKnownIssue("todo") {
+    //         try await CommonTests.test(input)
+    //     }
+    // }
+
+    // @Test(arguments: try JamTestnet.loadTests(path: "data/assurances/state_transitions", src: .jamduna))
+    // func assurancesTests(_ input: Testcase) async throws {
+    //     await withKnownIssue("todo") {
+    //         try await CommonTests.test(input)
+    //     }
+    // }
+}
