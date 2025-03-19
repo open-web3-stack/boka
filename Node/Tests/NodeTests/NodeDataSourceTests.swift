067ab76c298907a06605ec6dd108f90e2eaa2e2f
MacOMNI <414294494@qq.com>
2025-03-19 09:40:17 +0800

    update more tests

--- Node/Tests/NodeTests/NodeDataSourceTests.swift
+++ Node/Tests/NodeTests/NodeDataSourceTests.swift
@@ -54,4 +54,47 @@ struct NodeDataSourceTests {
         #expect(workPackageEvent.workPackage.value == workPackage)
         #expect(workPackageEvent.extrinsics == extrinsic)
     }
+
+    @Test func createKey() async throws {
+        let blsKey = try await dataSource.createKey(keyType: .BLS)
+        let ed25519Key = try await dataSource.createKey(keyType: .Ed25519)
+        let bandersnatchKey = try await dataSource.createKey(keyType: .Bandersnatch)
+
+        #expect(blsKey.count > 0)
+        #expect(ed25519Key.count > 0)
+        #expect(bandersnatchKey.count > 0)
+    }
+
+    @Test func listKeys() async throws {
+        let blsKey = try await dataSource.createKey(keyType: .BLS)
+        let ed25519Key = try await dataSource.createKey(keyType: .Ed25519)
+        let bandersnatchKey = try await dataSource.createKey(keyType: .Bandersnatch)
+
+        let keys = try await dataSource.listKeys()
+        #expect(keys.contains(blsKey))
+        #expect(keys.contains(ed25519Key))
+        #expect(keys.contains(bandersnatchKey))
+    }
+
+    @Test func hasKey() async throws {
+        let blsKeyHex = try await dataSource.createKey(keyType: .BLS)
+        let ed25519KeyHex = try await dataSource.createKey(keyType: .Ed25519)
+        let bandersnatchKeyHex = try await dataSource.createKey(keyType: .Bandersnatch)
+
+        let blsKeyData = Data(fromHexString: blsKeyHex)!
+        let ed25519KeyData = Data(fromHexString: ed25519KeyHex)!
+        let bandersnatchKeyData = Data(fromHexString: bandersnatchKeyHex)!
+
+        let hasBLSKey = try await dataSource.hasKey(publicKey: blsKeyData)
+        let hasEd25519Key = try await dataSource.hasKey(publicKey: ed25519KeyData)
+        let hasBandersnatchKey = try await dataSource.hasKey(publicKey: bandersnatchKeyData)
+
+        #expect(hasBLSKey == true)
+        #expect(hasEd25519Key == true)
+        #expect(hasBandersnatchKey == true)
+
+        let randomData = Data32.random().data
+        let hasRandomKey = try await dataSource.hasKey(publicKey: randomData)
+        #expect(hasRandomKey == false)
+    }
 }
