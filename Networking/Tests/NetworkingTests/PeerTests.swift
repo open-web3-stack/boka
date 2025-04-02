af19ffc88755b92a4aefec0e2d9db9f3bc2bd7fb
MacOMNI <414294494@qq.com>
2025-04-02 13:59:20 +0800

    update Connection rotation strategy

--- Networking/Tests/NetworkingTests/PeerTests.swift
+++ Networking/Tests/NetworkingTests/PeerTests.swift
@@ -129,7 +129,7 @@ struct PeerTests {
         typealias EphemeralHandler = MockEphemeralStreamHandler
     }
 
-    @Test("connectionRotationStrategy", .disabled("TODO: Fix this test"))
+    @Test
     func connectionRotationStrategy() async throws {
         var peers: [Peer<MockStreamHandler>] = []
         var handlers: [MockPresistentStreamHandler] = []
