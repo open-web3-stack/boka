89fab37821bddb24293920968e1bee97a38aed4c
Bryan <xlchen1291@gmail.com>
2026-01-21 20:24:24 +0000

    refactor: remove deprecated JIT fallback mode

--- PolkaVM/Sources/PolkaVM/ExecOutcome.swift
+++ PolkaVM/Sources/PolkaVM/ExecOutcome.swift
@@ -16,7 +16,6 @@ public enum ExitReason: Equatable, Sendable {
     case outOfGas
     case hostCall(UInt32)
     case pageFault(UInt32)
-    case fallback(pc: UInt32, registers: [UInt64], gasUsed: UInt64)
 
     // TODO: Review and refine these integer codes for JIT communication.
     // Especially for cases with associated values, a more complex ABI might be needed
@@ -38,7 +37,6 @@ public enum ExitReason: Equatable, Sendable {
         case .outOfGas: 5
         case let .hostCall(id): 6 + UInt64(id) << 32
         case let .pageFault(address): 7 + UInt64(address) << 32
-        case .fallback: 8  // Fallback - register state will be extracted separately
         }
     }
 
@@ -52,7 +50,6 @@ public enum ExitReason: Equatable, Sendable {
         case 5: return .outOfGas
         case 6: return .hostCall(UInt32(rawValue >> 32))
         case 7: return .pageFault(UInt32(rawValue >> 32))
-        case 8: return .fallback(pc: 0, registers: [], gasUsed: 0)  // Placeholder - actual values extracted from registers
         // JIT-specific panic reasons
         case 10: return .panic(.jitCompilationFailed)
         case 11: return .panic(.jitMemoryError)
--- PolkaVM/Sources/PolkaVM/Executors/VMExecutionResult.swift
+++ PolkaVM/Sources/PolkaVM/Executors/VMExecutionResult.swift
@@ -6,12 +6,10 @@ public struct VMExecutionResult: Sendable {
     public let exitReason: ExitReason
     public let gasUsed: Gas
     public let outputData: Data?
-    public let fallbackState: (pc: UInt32, registers: [UInt64], gasUsed: UInt64)?
 
-    public init(exitReason: ExitReason, gasUsed: Gas, outputData: Data?, fallbackState: (pc: UInt32, registers: [UInt64], gasUsed: UInt64)? = nil) {
+    public init(exitReason: ExitReason, gasUsed: Gas, outputData: Data? = nil) {
         self.exitReason = exitReason
         self.gasUsed = gasUsed
         self.outputData = outputData
-        self.fallbackState = fallbackState
     }
 }
