a672f76325e5ae3f76ed16a70d06e5b466d0bb4a
Bryan <xlchen1291@gmail.com>
2026-01-26 21:52:54 +0000

    fix: resolve JIT crash blockers by adding Codec dependency and fixing division by zero

--- PolkaVM/Package.swift
+++ PolkaVM/Package.swift
@@ -21,6 +21,7 @@ let package = Package(
     dependencies: [
         .package(path: "../Utils"),
         .package(path: "../TracingUtils"),
+        .package(path: "../Codec"),
         .package(url: "https://github.com/apple/swift-testing.git", branch: "6.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.6.0"),
     ],
@@ -30,6 +31,7 @@ let package = Package(
             dependencies: [
                 "Utils",
                 "TracingUtils",
+                "Codec",
                 "CppHelper",
                 .product(name: "Logging", package: "swift-log"),
             ],
@@ -39,7 +41,7 @@ let package = Package(
                     "-Xcc", "-std=c++20",
                     "-Xcc", "-I/usr/include/c++/13",
                     "-Xcc", "-I/usr/include/x86_64-linux-gnu/c++/13",
-                    "-Xcc", "-I/home/ubuntu/boka/PolkaVM/Sources/asmjit"
+                    "-Xcc", "-I/home/ubuntu/boka/PolkaVM/Sources/asmjit",
                 ]),
             ]
         ),
@@ -59,7 +61,7 @@ let package = Package(
                     "-Xcc", "-std=c++20",
                     "-Xcc", "-I/usr/include/c++/13",
                     "-Xcc", "-I/usr/include/x86_64-linux-gnu/c++/13",
-                    "-Xcc", "-I/home/ubuntu/boka/PolkaVM/Sources/asmjit"
+                    "-Xcc", "-I/home/ubuntu/boka/PolkaVM/Sources/asmjit",
                 ]),
             ]
         ),
@@ -68,6 +70,7 @@ let package = Package(
             dependencies: [
                 "PolkaVM",
                 "CppHelper",
+                "Codec",
                 .product(name: "Testing", package: "swift-testing"),
             ],
             swiftSettings: [
@@ -76,7 +79,7 @@ let package = Package(
                     "-Xcc", "-std=c++20",
                     "-Xcc", "-I/usr/include/c++/13",
                     "-Xcc", "-I/usr/include/x86_64-linux-gnu/c++/13",
-                    "-Xcc", "-I/home/ubuntu/boka/PolkaVM/Sources/asmjit"
+                    "-Xcc", "-I/home/ubuntu/boka/PolkaVM/Sources/asmjit",
                 ]),
             ]
         ),
@@ -84,8 +87,35 @@ let package = Package(
             name: "CppHelper",
             dependencies: [
             ],
-            exclude: ["asmjit/asmjit.natvis", "asmjit/include/asmjit.natvis", "asmjit/asmjit/asmjit.natvis", "asmjit/.git", "asmjit/.github", "asmjit/CMakeLists.txt", "asmjit/asmjit-testing", "asmjit/tools", "asmjit/db", "asmjit/module.modulemap", "asmjit/LICENSE.md", "asmjit/README.md", "asmjit/CONTRIBUTING.md", "asmjit/configure.sh", "asmjit/configure_sanitizers.sh", "asmjit/configure_vs2022_x86.bat", "asmjit/configure_vs2022_x64.bat", "asmjit/CMakePresets.json", "asmjit/include"],
-            sources: [".", "../asmjit/asmjit/core", "../asmjit/asmjit/arm", "../asmjit/asmjit/x86", "../asmjit/asmjit/support", "../asmjit/asmjit/ujit"],
+            exclude: [
+                "asmjit/asmjit.natvis",
+                "asmjit/include/asmjit.natvis",
+                "asmjit/asmjit/asmjit.natvis",
+                "asmjit/.git",
+                "asmjit/.github",
+                "asmjit/CMakeLists.txt",
+                "asmjit/asmjit-testing",
+                "asmjit/tools",
+                "asmjit/db",
+                "asmjit/module.modulemap",
+                "asmjit/LICENSE.md",
+                "asmjit/README.md",
+                "asmjit/CONTRIBUTING.md",
+                "asmjit/configure.sh",
+                "asmjit/configure_sanitizers.sh",
+                "asmjit/configure_vs2022_x86.bat",
+                "asmjit/configure_vs2022_x64.bat",
+                "asmjit/CMakePresets.json",
+                "asmjit/include",
+            ],
+            sources: [
+                ".",
+                "../asmjit/asmjit/core",
+                "../asmjit/asmjit/arm",
+                "../asmjit/asmjit/x86",
+                "../asmjit/asmjit/support",
+                "../asmjit/asmjit/ujit",
+            ],
             publicHeadersPath: ".",
             cxxSettings: [
                 .headerSearchPath("../../Sources/asmjit"),
@@ -93,7 +123,7 @@ let package = Package(
                     "-std=c++20",
                     "-I/usr/include/c++/13",
                     "-I/usr/include/x86_64-linux-gnu/c++/13",
-                    "-I/home/ubuntu/boka/PolkaVM/Sources/asmjit"
+                    "-I/home/ubuntu/boka/PolkaVM/Sources/asmjit",
                 ]),
                 .define("ASMJIT_STATIC"),
             ]
