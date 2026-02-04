8ddb6912a20f39d657639a774e79ce657d9d04d5
Bryan <xlchen1291@gmail.com>
2026-02-04 08:18:23 +0000

    fix

--- RPC/Tests/RPCTests/TelemetryHandlersTests.swift
+++ RPC/Tests/RPCTests/TelemetryHandlersTests.swift
@@ -21,51 +21,52 @@ struct TelemetryDummySource: TelemetryDataSource {
 }
 
 final class TelemetryHandlersTests {
-    var app: Application
+    var app: Application!
 
-    init() throws {
-        app = Application(.testing)
-
+    func setUp() async throws {
+        app = try await Application.make(.testing)
         let rpcController = JSONRPCController(
             handlers: TelemetryHandlers.getHandlers(source: TelemetryDummySource()),
         )
         try app.register(collection: rpcController)
     }
 
-    deinit {
-        app.shutdown()
-    }
-
-    @Test func name() throws {
+    @Test func name() async throws {
+        try await setUp()
         let req = JSONRequest(jsonrpc: "2.0", method: "telemetry_name", params: nil, id: 1)
         var buffer = ByteBuffer()
         try buffer.writeJSONEncodable(req)
-        try app.testable().test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res in
+        try await app.testable().test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res async in
             #expect(res.status == .ok)
-            let resp = try res.content.decode(JSONResponse.self, using: JSONDecoder())
+            let resp = try! res.content.decode(JSONResponse.self, using: JSONDecoder())
             #expect((resp.result!.value as! Utils.JSON).string == "TestNode")
         }
+        try await app.asyncShutdown()
     }
 
-    @Test func peersCount() throws {
+    @Test func peersCount() async throws {
+        try await setUp()
         let req = JSONRequest(jsonrpc: "2.0", method: "telemetry_peersCount", params: nil, id: 2)
         var buffer = ByteBuffer()
         try buffer.writeJSONEncodable(req)
-        try app.testable().test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res in
+        try await app.testable().test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res async in
             #expect(res.status == .ok)
-            let resp = try res.content.decode(JSONResponse.self, using: JSONDecoder())
+            let resp = try! res.content.decode(JSONResponse.self, using: JSONDecoder())
             #expect((resp.result!.value as! Utils.JSON).number == 42)
         }
+        try await app.asyncShutdown()
     }
 
-    @Test func networkKey() throws {
+    @Test func networkKey() async throws {
+        try await setUp()
         let req = JSONRequest(jsonrpc: "2.0", method: "telemetry_networkKey", params: nil, id: 3)
         var buffer = ByteBuffer()
         try buffer.writeJSONEncodable(req)
-        try app.testable().test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res in
+        try await app.testable().test(.POST, "/", headers: ["Content-Type": "application/json"], body: buffer) { res async in
             #expect(res.status == .ok)
-            let resp = try res.content.decode(JSONResponse.self, using: JSONDecoder())
+            let resp = try! res.content.decode(JSONResponse.self, using: JSONDecoder())
             #expect((resp.result!.value as! Utils.JSON).string == "Ed25519:TestKey")
         }
+        try await app.asyncShutdown()
     }
 }
